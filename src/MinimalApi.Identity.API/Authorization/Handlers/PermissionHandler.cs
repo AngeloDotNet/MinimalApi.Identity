03ef38d4fba141bad37f8652a2c6e98471d0bc7e
Angelo Pirola <angelo9871@gmail.com>
2025-06-02 10:01:24 +0200

    Add permission handling for user authorization
    Updated `PermissionHandler` to implement authorization logic
    based on user permissions, including checks for authentication,
    lockout status, and security stamp validation. Introduced
    `PermissionRequirement` class to define required permissions
    for authorization checks.

---
+++ src/MinimalApi.Identity.API/Authorization/Handlers/PermissionHandler.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using MinimalApi.Identity.API.Constants;
+using MinimalApi.Identity.API.Entities;
+using MinimalApi.Identity.API.Exceptions.Users;
+using MinimalApi.Identity.API.Extensions;
+using MinimalApi.Identity.BusinessLayer.Authorization.Requirement;
+
+namespace MinimalApi.Identity.API.Authorization.Handlers;
+
+public class PermissionHandler(ILogger<PermissionHandler> logger, UserManager<ApplicationUser> userManager) : IAuthorizationHandler
+{
+    public async Task HandleAsync(AuthorizationHandlerContext context)
+    {
+        var user = context.User;
+        var permissionsRequirements = context.Requirements.OfType<PermissionRequirement>();
+
+        if (UsersExtensions.IsAuthenticated(user))
+        {
+            var userId = user.GetUserId();
+            var nameUser = user?.Identity?.Name;
+            var securityStamp = context.User.GetClaimValue(ClaimTypes.SerialNumber);
+            var utente = await userManager.FindByIdAsync(userId);
+
+            if (utente == null || user == null)
+            {
+                var message = $"User {nameUser} not found";
+
+                logger.LogWarning(message);
+                throw new UserUnknownException(message);
+            }
+
+            if (utente.LockoutEnd.GetValueOrDefault() > DateTimeOffset.UtcNow)
+            {
+                var message = MessageApi.UserLockedOut;
+
+                logger.LogWarning(message);
+                throw new UserIsLockedException(message);
+            }
+
+            if (securityStamp != utente.SecurityStamp)
+            {
+                var message = $"User {nameUser} security stamp is invalid";
+
+                logger.LogWarning(message);
+                throw new UserTokenIsInvalidException(message);
+            }
+
+            foreach (var permissionRequirement in permissionsRequirements)
+            {
+                if (permissionRequirement.Permissions.All(permission => user.HasClaim(CustomClaimTypes.Permission, permission)))
+                {
+                    context.Succeed(permissionRequirement);
+                }
+                else
+                {
+                    var message = $"User {nameUser} does not have the required permissions";
+
+                    logger.LogWarning(message);
+                    throw new UserWithoutPermissionsException(message);
+                }
+            }
+        }
+
+        await Task.CompletedTask;
+    }
+}
