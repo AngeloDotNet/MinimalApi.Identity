fafb72d364f3ce85b08fafcd2f571a8d1fbb0417
Angelo Pirola <angelo9871@gmail.com>
2025-06-02 10:01:46 +0200

    Add Entity Framework configurations for identity entities
    This commit introduces several configuration classes for Entity Framework Core within the `MinimalApi.Identity.API.Database.Configurations` namespace. Key changes include:
    
    - `ApplicationRoleConfiguration`: Seeds data for roles (Admin, PowerUser, User) with relevant properties.
    - `ApplicationUserRoleConfiguration`: Sets up composite keys and relationships for user-role associations.
    - `AuthPolicyConfiguration`: Seeds various authentication policies with detailed properties.
    - `ClaimTypeConfiguration`: Seeds claim types and their default statuses.
    - `EmailSendingConfiguration`: Configures the `EmailSending` entity with type conversion.
    - `UserLicenseConfiguration` and `UserModuleConfiguration`: Define composite keys and relationships for user licenses and modules.
    - Updated `MinimalApiAuthDbContext` to include new DbSet properties and apply configurations from the assembly.
    
    These changes enhance the database schema and relationships for the identity management system.

---
+++ src/MinimalApi.Identity.API/Database/MinimalApiAuthDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Identity.API.Entities;
+
+namespace MinimalApi.Identity.API.Database;
+
+public class MinimalApiAuthDbContext(DbContextOptions<MinimalApiAuthDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole,
+    int, IdentityUserClaim<int>, ApplicationUserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>(options)
+{
+    public DbSet<License> Licenses { get; set; }
+    public DbSet<UserLicense> UserLicenses { get; set; }
+    public DbSet<Entities.Module> Modules { get; set; }
+    public DbSet<UserModule> UserModules { get; set; }
+    public DbSet<EmailSending> EmailSendings { get; set; }
+    public DbSet<ClaimType> ClaimTypes { get; set; }
+    public DbSet<UserProfile> UserProfiles { get; set; }
+    public DbSet<AuthPolicy> AuthPolicies { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
