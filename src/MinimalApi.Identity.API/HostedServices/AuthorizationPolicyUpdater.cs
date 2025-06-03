21a4b485342162bfb5558744ea7f997c3485c78a
Angelo Pirola <angelo9871@gmail.com>
2025-06-03 08:49:51 +0200

    Add hosted services for authorization policy management
    Introduce `AuthorizationPolicyGeneration` and `AuthorizationPolicyUpdater` classes implementing `IHostedService`. These classes handle the asynchronous generation and periodic updating of authorization policies, utilizing dependency injection for service access. Both classes include logging for success and failure, along with error handling and resource management.

---
+++ src/MinimalApi.Identity.API/HostedServices/AuthorizationPolicyUpdater.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MinimalApi.Identity.API.Options;
+using MinimalApi.Identity.API.Services.Interfaces;
+
+namespace MinimalApi.Identity.API.HostedServices;
+
+public class AuthorizationPolicyUpdater(IServiceProvider serviceProvider, ILogger<AuthorizationPolicyUpdater> logger,
+    IOptions<HostedServiceOptions> hostedOptions) : IHostedService, IDisposable
+{
+    private Timer? timer;
+    private readonly HostedServiceOptions options = hostedOptions.Value;
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        timer = new Timer(UpdateAuthorizationPolicyAsync, null, TimeSpan.Zero, TimeSpan.FromMinutes(options.IntervalAuthPolicyUpdaterMinutes));
+        return Task.CompletedTask;
+    }
+
+    private async void UpdateAuthorizationPolicyAsync(object? state)
+    {
+        try
+        {
+            using var scope = serviceProvider.CreateScope();
+            var authPolicyService = scope.ServiceProvider.GetRequiredService<IAuthPolicyService>();
+            var result = await authPolicyService.UpdateAuthPoliciesAsync();
+
+            if (result)
+            {
+                logger.LogInformation("Authorization policies updated successfully.");
+            }
+            else
+            {
+                logger.LogWarning("An error occurred while generating authorization policies.");
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An exception occurred while updating authorization policies.");
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        timer?.Change(Timeout.Infinite, 0);
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        timer?.Dispose();
+    }
+}
