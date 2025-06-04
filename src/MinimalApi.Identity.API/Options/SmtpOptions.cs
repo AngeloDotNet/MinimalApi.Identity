004a461abd52c1980ffb01481ab185e658a6df19
Angelo Pirola <angelo9871@gmail.com>
2025-06-04 10:52:55 +0200

    Add configuration classes for API and identity options
    Introduced several classes to define options for API validation, JWT settings, hosted services, SMTP configurations, and user management. Each class includes properties with validation attributes to enforce rules such as required fields, ranges, and specific formats.
    
    - ApiValidationOptions: Properties for identifier length validation.
    - HostedServiceOptions: Interval for authentication policy updater.
    - JwtOptions: Properties for issuer, audience, security key, and token expiration.
    - NetIdentityOptions: Identity requirements including password complexity.
    - SmtpOptions: SMTP configuration properties with validation.
    - UsersOptions: Admin role assignment and password expiration settings.
    - ValidationOptions: Error response format property for API responses.

---
+++ src/MinimalApi.Identity.API/Options/SmtpOptions.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MailKit.Security;
+using MinimalApi.Identity.API.Validator.Attribute;
+
+namespace MinimalApi.Identity.API.Options;
+
+public class SmtpOptions
+{
+    [Required(ErrorMessage = "Host is required.")]
+    public string Host { get; init; } = null!;
+
+    [IntegerInList(25, 587, 465, ErrorMessage = "Port must be one of the following: 25, 587, 465.")]
+    public int Port { get; init; }
+
+    [Required(ErrorMessage = "Security option is required.")]
+    public SecureSocketOptions Security { get; init; }
+
+    [Required(ErrorMessage = "Username is required.")]
+    public string Username { get; init; } = null!;
+
+    [Required(ErrorMessage = "Password is required.")]
+    public string Password { get; init; } = null!;
+
+    [Required(ErrorMessage = "Sender is required.")]
+    [EmailAddress(ErrorMessage = "Sender must be a valid email address.")]
+    public string Sender { get; init; } = null!;
+
+    public bool SaveEmailSent { get; init; }
+}
