5078e22e8c852621ce9464e21a408d823d85897b
Angelo Pirola <angelo9871@gmail.com>
2025-06-05 16:36:23 +0200

    Add module validators for input validation
    Introduce four new validator classes: `AssignModuleValidator`, `CreateModuleValidator`, `DeleteModuleValidator`, and `RevokeModuleValidator`. These classes enforce validation rules for required fields and numeric constraints on model properties. The `CreateModuleValidator` also utilizes options for minimum and maximum lengths of `Name` and `Description`. This enhances input validation for module-related operations in the API.

---
+++ src/MinimalApi.Identity.API/Validator/Modules/CreateModuleValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Options;
+using MinimalApi.Identity.API.Models;
+using MinimalApi.Identity.API.Options;
+
+namespace MinimalApi.Identity.API.Validator.Modules;
+
+public class CreateModuleValidator : AbstractValidator<CreateModuleModel>
+{
+    public CreateModuleValidator(IOptions<ApiValidationOptions> options)
+    {
+        var validationOptions = options.Value;
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required")
+            .MinimumLength(validationOptions.MinLengthModuleName).WithMessage($"Name must be at least {validationOptions.MinLengthModuleName} characters")
+            .MaximumLength(validationOptions.MaxLengthModuleName).WithMessage($"Name must not exceed {validationOptions.MaxLengthModuleName} characters");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required")
+            .MinimumLength(validationOptions.MinLengthModuleDescription).WithMessage($"Name must be at least {validationOptions.MinLengthModuleDescription} characters")
+            .MaximumLength(validationOptions.MaxLengthModuleDescription).WithMessage($"Name must not exceed {validationOptions.MaxLengthModuleDescription} characters");
+    }
+}
