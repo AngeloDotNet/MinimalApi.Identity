88c86da4c1f1902cff92071b5d59eeae7ccd1e79
Angelo Pirola <angelo9871@gmail.com>
2025-06-05 16:40:03 +0200

    Refactor MinimalApiAuthDbContext for dynamic entity reg
    - Added using directive for common entity definitions.
    - Commented out DbSet properties for future reference.
    - Implemented dynamic entity registration in OnModelCreating.
    - Retained configuration application from the assembly.

--- src/MinimalApi.Identity.API/Database/MinimalApiAuthDbContext.cs
+++ src/MinimalApi.Identity.API/Database/MinimalApiAuthDbContext.cs
@@ -3,24 +3,33 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using MinimalApi.Identity.API.Entities;
+using MinimalApi.Identity.API.Entities.Common;
 
 namespace MinimalApi.Identity.API.Database;
 
 public class MinimalApiAuthDbContext(DbContextOptions<MinimalApiAuthDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole,
     int, IdentityUserClaim<int>, ApplicationUserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>(options)
 {
-    public DbSet<License> Licenses { get; set; }
-    public DbSet<UserLicense> UserLicenses { get; set; }
-    public DbSet<Entities.Module> Modules { get; set; }
-    public DbSet<UserModule> UserModules { get; set; }
-    public DbSet<EmailSending> EmailSendings { get; set; }
-    public DbSet<ClaimType> ClaimTypes { get; set; }
-    public DbSet<UserProfile> UserProfiles { get; set; }
-    public DbSet<AuthPolicy> AuthPolicies { get; set; }
+    //public DbSet<License> Licenses { get; set; }
+    //public DbSet<UserLicense> UserLicenses { get; set; }
+    //public DbSet<Entities.Module> Modules { get; set; }
+    //public DbSet<UserModule> UserModules { get; set; }
+    //public DbSet<EmailSending> EmailSendings { get; set; }
+    //public DbSet<ClaimType> ClaimTypes { get; set; }
+    //public DbSet<UserProfile> UserProfiles { get; set; }
+    //public DbSet<AuthPolicy> AuthPolicies { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        var entityTypes = Assembly.GetExecutingAssembly().GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract && typeof(IEntity).IsAssignableFrom(t));
+
+        foreach (var type in entityTypes)
+        {
+            builder.Entity(type);
+        }
 
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
