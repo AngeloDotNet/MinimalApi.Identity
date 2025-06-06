93f67b2eb6e760d15e704be05663bd06bb0e000a
Angelo Pirola <angelo9871@gmail.com>
2025-06-06 09:12:13 +0200

    Clean up MinimalApiAuthDbContext and improve formatting
    Removed unused commented-out DbSet properties from the
    MinimalApiAuthDbContext class. Adjusted formatting for
    the assignment of the entityTypes variable for better
    readability without changing the underlying logic.

--- src/MinimalApi.Identity.API/Database/MinimalApiAuthDbContext.cs
+++ src/MinimalApi.Identity.API/Database/MinimalApiAuthDbContext.cs
@@ -10,21 +10,12 @@ namespace MinimalApi.Identity.API.Database;
 public class MinimalApiAuthDbContext(DbContextOptions<MinimalApiAuthDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole,
     int, IdentityUserClaim<int>, ApplicationUserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>(options)
 {
-    //public DbSet<License> Licenses { get; set; }
-    //public DbSet<UserLicense> UserLicenses { get; set; }
-    //public DbSet<Entities.Module> Modules { get; set; }
-    //public DbSet<UserModule> UserModules { get; set; }
-    //public DbSet<EmailSending> EmailSendings { get; set; }
-    //public DbSet<ClaimType> ClaimTypes { get; set; }
-    //public DbSet<UserProfile> UserProfiles { get; set; }
-    //public DbSet<AuthPolicy> AuthPolicies { get; set; }
-
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
-        var entityTypes = Assembly.GetExecutingAssembly().GetTypes()
-            .Where(t => t.IsClass && !t.IsAbstract && typeof(IEntity).IsAssignableFrom(t));
+        var entityTypes = Assembly.GetExecutingAssembly()
+            .GetTypes().Where(t => t.IsClass && !t.IsAbstract && typeof(IEntity).IsAssignableFrom(t));
 
         foreach (var type in entityTypes)
         {
