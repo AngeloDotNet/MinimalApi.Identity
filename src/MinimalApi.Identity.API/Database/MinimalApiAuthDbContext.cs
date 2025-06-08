697def5b6448577714833fe2a0507b2d1bcf7526
Angelo Pirola <angelo9871@gmail.com>
2025-06-08 19:53:53 +0200

    Refactor MinimalApiAuthDbContext namespaces and types
    Updated entity namespaces in MinimalApiAuthDbContext to
    use MinimalApi.Identity.Core.Entities. Modified the
    constructor signature to remove ApplicationRole from
    IdentityDbContext's generic parameters, streamlining
    the identity-related entity organization.

--- src/MinimalApi.Identity.API/Database/MinimalApiAuthDbContext.cs
+++ src/MinimalApi.Identity.API/Database/MinimalApiAuthDbContext.cs
@@ -2,13 +2,14 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using MinimalApi.Identity.API.Entities;
-using MinimalApi.Identity.API.Entities.Common;
+using MinimalApi.Identity.Core.Entities;
+using MinimalApi.Identity.Core.Entities.Common;
 
 namespace MinimalApi.Identity.API.Database;
 
-public class MinimalApiAuthDbContext(DbContextOptions<MinimalApiAuthDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole,
-    int, IdentityUserClaim<int>, ApplicationUserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>(options)
+public class MinimalApiAuthDbContext(DbContextOptions<MinimalApiAuthDbContext> options) : IdentityDbContext<ApplicationUser,
+    ApplicationRole, int, IdentityUserClaim<int>, ApplicationUserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>,
+    IdentityUserToken<int>>(options)
 {
     protected override void OnModelCreating(ModelBuilder builder)
     {
