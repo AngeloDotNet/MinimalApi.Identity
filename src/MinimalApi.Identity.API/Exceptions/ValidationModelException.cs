af0beda2d4c6b160db59f39b2f0d1b87a618d849
Angelo Pirola <angelo9871@gmail.com>
2025-06-15 11:17:46 +0200

    Refactor ValidationModelException class
    Removed namespace declaration and simplified constructor.
    Retained Errors property, now initialized directly in the constructor.

--- src/MinimalApi.Identity.API/Exceptions/ValidationModelException.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MinimalApi.Identity.API.Exceptions;
-
-public class ValidationModelException(string message, IDictionary<string, string[]> errors) : Exception(message)
-{
-    public IDictionary<string, string[]> Errors { get; } = errors;
-}
