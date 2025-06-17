c1f85308416a37bbbb0d2028b5963acedb08cf9a
Angelo Pirola <angelo9871@gmail.com>
2025-06-17 08:59:39 +0200

    Add ServiceRegistrationConfiguration class
    Introduces the `ServiceRegistrationConfiguration` class that
    accepts an `IServiceCollection` in its constructor. It initializes
    a read-only `Services` property and includes properties for
    `Interfaces`, `StringEndsWith`, and `Lifetime` with default
    values. Removed commented-out code and corrected syntax.

---
+++ src/MinimalApi.Identity.Core/Configurations/ServiceRegistrationConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MinimalApi.Identity.Core.Configurations;
+
+//public class ServiceRegistrationConfiguration(IServiceCollection services)
+//{
+//    private readonly IServiceCollection services = services;
+
+//    public List<Type> Interfaces { get; set; } = [];
+//    public string StringEndsWith { get; set; } = "Service";
+//    public ServiceLifetime Lifetime { get; set; } = ServiceLifetime.Transient; // valore di default
+//}
+
+public class ServiceRegistrationConfiguration(IServiceCollection services)
+{
+    public IServiceCollection Services { get; } = services ?? throw new ArgumentNullException(nameof(services));
+    public List<Type> Interfaces { get; set; } = [];
+    public string StringEndsWith { get; set; } = "Service";
+    public ServiceLifetime Lifetime { get; set; } = ServiceLifetime.Transient;
+}
