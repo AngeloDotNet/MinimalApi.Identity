184ff4445b20c53cbc8f43817bb70eafa4845fdf
Angelo Pirola <angelo9871@gmail.com>
2025-06-18 08:56:10 +0200

    Add ServiceCoreExtensions for service configuration
    Introduces a new static class `ServiceCoreExtensions` in the
    `MinimalApi.Identity.Core.DependencyInjection` namespace. This class
    provides extension methods for service configuration and validation,
    including methods for producing problem responses, retrieving OpenAPI
    responses by status code, and configuring FluentValidation. The
    `AddRegisterServices` method supports service registration with
    various lifetimes (Singleton, Scoped, Transient) and includes a
    utility to find implementation interfaces for given types.

---
+++ src/MinimalApi.Identity.Core/DependencyInjection/ServiceCoreExtensions.cs
@@ -0,0 +1,86 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using MinimalApi.Identity.Core.Configurations;
+using MinimalApi.Identity.Core.Filters;
+using MinimalApi.Identity.Core.Options;
+
+namespace MinimalApi.Identity.Core.DependencyInjection;
+
+public static class ServiceCoreExtensions
+{
+    public const string UserNotFound = "User not found";
+
+    public static RouteHandlerBuilder ProducesDefaultProblem(this RouteHandlerBuilder builder, params int[] statusCodes)
+    {
+        foreach (var statusCode in statusCodes)
+        {
+            builder.ProducesProblem(statusCode);
+        }
+
+        return builder;
+    }
+
+    public static OpenApiResponse Response(this OpenApiOperation operation, int statusCode)
+        => operation.Responses.GetByStatusCode(statusCode);
+
+    public static OpenApiResponse GetByStatusCode(this OpenApiResponses responses, int statusCode)
+        => responses.Single(r => r.Key == statusCode.ToString()).Value;
+
+    public static RouteHandlerBuilder WithValidation<T>(this RouteHandlerBuilder builder) where T : class
+        => builder.AddEndpointFilter<ValidatorFilter<T>>().ProducesValidationProblem();
+
+    public static IServiceCollection ConfigureFluentValidation<TValidator>(this IServiceCollection services) where TValidator : IValidator
+        => services.AddValidatorsFromAssembly(typeof(TValidator).Assembly);
+
+    public static IServiceCollection ConfigureValidation(this IServiceCollection services, Action<ValidationOptions> configureOptions)
+        => services.Configure(configureOptions);
+
+    public static IServiceCollection AddRegisterServices(this IServiceCollection services, Action<ServiceRegistrationConfiguration> configure)
+    {
+        var configuration = new ServiceRegistrationConfiguration(services);
+        configure.Invoke(configuration);
+
+        foreach (var interfaceType in configuration.Interfaces)
+        {
+            var assembly = FindImplementationInterfaces(interfaceType);
+
+            services.Scan(scan =>
+            {
+                var classSelector = scan.FromAssemblies(assembly)
+                    .AddClasses(classes => classes.Where(type => type.Name.EndsWith(configuration.StringEndsWith)))
+                    .AsImplementedInterfaces();
+
+                switch (configuration.Lifetime)
+                {
+                    case ServiceLifetime.Singleton:
+                        classSelector.WithSingletonLifetime();
+                        break;
+                    case ServiceLifetime.Scoped:
+                        classSelector.WithScopedLifetime();
+                        break;
+                    case ServiceLifetime.Transient:
+                        classSelector.WithTransientLifetime();
+                        break;
+                    default:
+                        classSelector.WithTransientLifetime();
+                        break;
+                }
+            });
+        }
+
+        return services;
+    }
+
+    internal static Assembly FindImplementationInterfaces(Type interfaceType)
+    {
+        var implementationType = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(a => a.GetTypes()).FirstOrDefault(t => interfaceType.IsAssignableFrom(t) && t.IsClass && !t.IsAbstract)
+            ?? throw new InvalidOperationException($"No implementation found for interface {interfaceType.FullName}");
+
+        return interfaceType.Assembly;
+    }
+}
