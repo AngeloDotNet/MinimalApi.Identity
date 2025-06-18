9f1d55b82a7803c39e44bb8954610bd463f7c13b
Angelo Pirola <angelo9871@gmail.com>
2025-06-18 08:55:46 +0200

    Add endpoint mapping extensions and handler interface
    Introduce extension methods for `IEndpointRouteBuilder` to map endpoints from a specified assembly or the assembly containing a specified type. Implement reflection to find and invoke `MapEndpoints` methods from classes implementing the new `IEndpointRouteHandlerBuilder` interface, which is also defined in this commit.

---
+++ src/MinimalApi.Identity.Core/Extensions/IEndpointRouteHandlerBuilder.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace MinimalApi.Identity.Core.Extensions;
+
+public interface IEndpointRouteHandlerBuilder
+{
+    public static abstract void MapEndpoints(IEndpointRouteBuilder endpoints);
+}
