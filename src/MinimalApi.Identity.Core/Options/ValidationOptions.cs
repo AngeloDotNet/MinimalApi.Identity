01a6e65e6cacfd0392207ac3b430197eabab25c5
Angelo Pirola <angelo9871@gmail.com>
2025-06-18 08:55:23 +0200

    Add validation options for API entities
    Introduce `ApiValidationOptions` class with properties for validating first names, last names, usernames, role names, module names, license names, claim values, and policy names, including necessary validation attributes. Also, add `ValidationOptions` class to specify error response format.

---
+++ src/MinimalApi.Identity.Core/Options/ValidationOptions.cs
@@ -0,0 +1,8 @@
+﻿using MinimalApi.Identity.Core.Enums;
+
+namespace MinimalApi.Identity.Core.Options;
+
+public class ValidationOptions
+{
+    public ErrorResponseFormat ErrorResponseFormat { get; set; }
+}
