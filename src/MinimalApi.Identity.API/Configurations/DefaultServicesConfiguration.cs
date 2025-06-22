4ef20ced01fdc1f14592953c2a550247ed611d81
Angelo Pirola <angelo9871@gmail.com>
2025-06-22 08:56:52 +0200

    Add DefaultServicesConfiguration class for DI setup
    Introduces the `DefaultServicesConfiguration` class in the
    `MinimalApi.Identity.API.Configurations` namespace. This class
    accepts an `IServiceCollection` in its constructor and includes
    properties for configuration, database connection string, and
    error response format with default values. Necessary namespaces
    for dependency injection and configuration management are also
    imported.

---
+++ src/MinimalApi.Identity.API/Configurations/DefaultServicesConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MinimalApi.Identity.Core.Enums;
+
+namespace MinimalApi.Identity.API.Configurations;
+
+public class DefaultServicesConfiguration(IServiceCollection services)
+{
+    private readonly IServiceCollection services = services;
+
+    public IConfiguration Configure { get; set; } = null!; // Must be set before using
+    public string DatabaseConnectionString { get; set; } = null!; // Must be set before using
+    public ErrorResponseFormat FormatErrorResponse { get; set; } = ErrorResponseFormat.Default; // Default format for error responses
+}
