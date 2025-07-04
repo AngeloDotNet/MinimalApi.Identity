4e66e87ccddcec156f77432b5ba7c9a146eb7197
Angelo Pirola <angelo9871@gmail.com>
2025-07-04 08:52:29 +0200

    Remove unused usings and options validation method
    This commit removes the using directives for
    `Microsoft.Extensions.Configuration`, `Microsoft.Extensions.DependencyInjection`,
    and `Microsoft.Extensions.Options`. Additionally, the method
    `AddOptionValidate<TOptions>` has been removed from the
    `ServicesExtensions` class, simplifying the code while
    keeping the `GetDatabaseConnString` method intact.

--- src/MinimalApi.Identity.API/Extensions/ServicesExtensions.cs
+++ src/MinimalApi.Identity.API/Extensions/ServicesExtensions.cs
@@ -1,21 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 
 namespace MinimalApi.Identity.API.Extensions;
 
 public static class ServicesExtensions
 {
-    //TODO: code cleanup
-    public static TOptions AddOptionValidate<TOptions>(this IServiceCollection services, string sectionName) where TOptions : class
-    {
-        services.AddOptions<TOptions>().BindConfiguration(sectionName).ValidateDataAnnotations().ValidateOnStart();
-
-        using var serviceProvider = services.BuildServiceProvider();
-
-        return serviceProvider.GetRequiredService<IOptions<TOptions>>().Value;
-    }
-
     public static string GetDatabaseConnString(this IConfiguration configuration, string sectionName)
     {
         return configuration.GetConnectionString(sectionName)
