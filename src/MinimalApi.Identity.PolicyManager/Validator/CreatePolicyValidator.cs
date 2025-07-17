ef9122af3d0f3b1c254bd6326128e30122e604cb
Angelo Pirola <angelo9871@gmail.com>
2025-07-17 08:40:57 +0200

    Add validation for Create and Delete policy models
    Updated `CreatePolicyValidator` to enforce rules on `PolicyName`, `PolicyDescription`, and `PolicyPermissions` using options for length constraints. Enhanced `DeletePolicyValidator` to validate that `Id` is greater than zero and `PolicyName` is not empty.

---
+++ src/MinimalApi.Identity.PolicyManager/Validator/CreatePolicyValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Options;
+using MinimalApi.Identity.Core.Options;
+using MinimalApi.Identity.PolicyManager.Models;
+
+namespace MinimalApi.Identity.PolicyManager.Validator;
+
+public class CreatePolicyValidator : AbstractValidator<CreatePolicyModel>
+{
+    public CreatePolicyValidator(IOptions<ApiValidationOptions> options)
+    {
+        var validationOptions = options.Value;
+
+        RuleFor(x => x.PolicyName)
+            .NotEmpty().WithMessage("Name is required.")
+            .MinimumLength(validationOptions.MinLengthPolicyName).WithMessage($"Name must be at least {validationOptions.MinLengthPolicyName} characters")
+            .MaximumLength(validationOptions.MaxLengthPolicyName).WithMessage($"Name must not exceed {validationOptions.MaxLengthPolicyName} characters");
+
+        RuleFor(x => x.PolicyDescription)
+            .NotEmpty().WithMessage("Description is required.")
+            .MinimumLength(validationOptions.MinLengthPolicyDescription).WithMessage($"Name must be at least {validationOptions.MinLengthPolicyDescription} characters")
+            .MaximumLength(validationOptions.MaxLengthPolicyDescription).WithMessage($"Name must not exceed {validationOptions.MaxLengthPolicyDescription} characters");
+
+        RuleFor(x => x.PolicyPermissions)
+            .NotEmpty().WithMessage("Permissions are required.")
+            .Must(permissions => permissions != null && permissions.Length != 0).WithMessage("Permissions array must contain at least one element.");
+    }
+}
