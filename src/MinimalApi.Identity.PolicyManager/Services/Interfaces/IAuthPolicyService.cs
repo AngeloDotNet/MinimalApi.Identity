6b1cc3671874f15a26ca9ab183f9c3de07eef727
Angelo Pirola <angelo9871@gmail.com>
2025-07-18 08:51:28 +0200

    Add IAuthPolicyService interface for policy management
    This commit introduces the `IAuthPolicyService` interface within the `MinimalApi.Identity.PolicyManager.Services.Interfaces` namespace. It defines several asynchronous methods for managing policies, including retrieving, creating, deleting, generating, and updating authentication policies, as well as filtering authentication policies.

---
+++ src/MinimalApi.Identity.PolicyManager/Services/Interfaces/IAuthPolicyService.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using MinimalApi.Identity.Core.Entities;
+using MinimalApi.Identity.PolicyManager.Models;
+
+namespace MinimalApi.Identity.PolicyManager.Services.Interfaces;
+
+public interface IAuthPolicyService
+{
+    Task<List<PolicyResponseModel>> GetAllPoliciesAsync();
+    Task<string> CreatePolicyAsync(CreatePolicyModel model);
+    Task<string> DeletePolicyAsync(DeletePolicyModel model);
+    Task<bool> GenerateAuthPoliciesAsync();
+    Task<bool> UpdateAuthPoliciesAsync();
+    Task<List<AuthPolicy>> GetAllAuthPoliciesAsync(Expression<Func<AuthPolicy, bool>> filter = null!);
+}
