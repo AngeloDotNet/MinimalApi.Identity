98637357794ab2462459315a64a8c12b96b84e24
Angelo Pirola <angelo9871@gmail.com>
2025-07-18 08:52:01 +0200

    Add AuthPolicyEndpoints for managing authorization policies
    Introduces the `AuthPolicyEndpoints` class implementing `IEndpointRouteHandlerBuilder`.
    Sets up GET, POST, and DELETE endpoints for authorization policies with appropriate
    authorization requirements, response types, and OpenAPI documentation.
    Includes necessary using directives for ASP.NET Core and related identity features.

---
+++ src/MinimalApi.Identity.PolicyManager/Endpoints/AuthPolicyEndpoints.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.OpenApi.Models;
+using MinimalApi.Identity.Core.DependencyInjection;
+using MinimalApi.Identity.Core.Enums;
+using MinimalApi.Identity.Core.Extensions;
+using MinimalApi.Identity.PolicyManager.DependencyInjection;
+using MinimalApi.Identity.PolicyManager.Models;
+using MinimalApi.Identity.PolicyManager.Services.Interfaces;
+
+namespace MinimalApi.Identity.PolicyManager.Endpoints;
+
+public class AuthPolicyEndpoints : IEndpointRouteHandlerBuilder
+{
+    public static void MapEndpoints(IEndpointRouteBuilder endpoints)
+    {
+        var apiGroup = endpoints
+            .MapGroup(PolicyExtensions.EndpointsAuthPolicyGroup)
+            .RequireAuthorization()
+            .WithOpenApi(opt =>
+            {
+                opt.Tags = [new OpenApiTag { Name = PolicyExtensions.EndpointsAuthPolicyTag }];
+                return opt;
+            });
+
+        apiGroup.MapGet(PolicyExtensions.EndpointsStringEmpty, async ([FromServices] IAuthPolicyService authPolicyService) =>
+        {
+            return await authPolicyService.GetAllPoliciesAsync();
+        })
+        .Produces<Ok<List<PolicyResponseModel>>>(StatusCodes.Status200OK)
+        .ProducesDefaultProblem(StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound)
+        .RequireAuthorization(nameof(Permissions.AuthPolicyRead))
+        .WithOpenApi(opt =>
+        {
+            opt.Description = "Get all authorization policy";
+            opt.Summary = "Get all authorization policy";
+
+            opt.Response(StatusCodes.Status200OK).Description = "Claims retrieved successfully";
+            opt.Response(StatusCodes.Status401Unauthorized).Description = "Unauthorized";
+            opt.Response(StatusCodes.Status404NotFound).Description = "Not found";
+
+            return opt;
+        });
+
+        apiGroup.MapPost(PolicyExtensions.EndpointsCreateAuthPolicy, async ([FromServices] IAuthPolicyService authPolicyService,
+            [FromBody] CreatePolicyModel inputModel) =>
+        {
+            return await authPolicyService.CreatePolicyAsync(inputModel);
+        })
+        .Produces<Ok<string>>(StatusCodes.Status200OK)
+        .ProducesDefaultProblem(StatusCodes.Status400BadRequest, StatusCodes.Status401Unauthorized, StatusCodes.Status422UnprocessableEntity)
+        .RequireAuthorization(nameof(Permissions.AuthPolicyWrite))
+        .WithValidation<CreatePolicyModel>()
+        .WithOpenApi(opt =>
+        {
+            opt.Description = "Add authorization policy";
+            opt.Summary = "Add authorization policy";
+
+            opt.Response(StatusCodes.Status200OK).Description = "Policy added successfully";
+            opt.Response(StatusCodes.Status400BadRequest).Description = "Bad Request";
+            opt.Response(StatusCodes.Status401Unauthorized).Description = "Unauthorized";
+
+            return opt;
+        });
+
+        apiGroup.MapDelete(PolicyExtensions.EndpointsDeleteAuthPolicy, async ([FromServices] IAuthPolicyService authPolicyService,
+            [FromBody] DeletePolicyModel inputModel) =>
+        {
+            return await authPolicyService.DeletePolicyAsync(inputModel);
+        })
+        .Produces<Ok<string>>(StatusCodes.Status200OK)
+        .ProducesDefaultProblem(StatusCodes.Status400BadRequest, StatusCodes.Status401Unauthorized, StatusCodes.Status422UnprocessableEntity)
+        .RequireAuthorization(nameof(Permissions.AuthPolicyWrite))
+        .WithValidation<DeletePolicyModel>()
+        .WithOpenApi(opt =>
+        {
+            opt.Description = "Delete authorization policy";
+            opt.Summary = "Delete authorization policy";
+
+            opt.Response(StatusCodes.Status200OK).Description = "Policy deleted successfully";
+            opt.Response(StatusCodes.Status400BadRequest).Description = "Bad Request";
+            opt.Response(StatusCodes.Status401Unauthorized).Description = "Unauthorized";
+
+            return opt;
+        });
+    }
+}
