ae34aca54284f72d9b14152e9a914a0a51d81635
Angelo Pirola <angelo9871@gmail.com>
2025-07-19 09:20:16 +0200

    Remove HostedServiceOptions class and its validation
    This commit removes the `HostedServiceOptions` class, including the `IntervalAuthPolicyUpdaterMinutes` property and its associated validation attribute. The `using System.ComponentModel.DataAnnotations;` directive has also been deleted as it is no longer needed.

--- src/MinimalApi.Identity.API/Options/HostedServiceOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace MinimalApi.Identity.API.Options;
-
-public class HostedServiceOptions
-{
-    [Required, Range(1, int.MaxValue, ErrorMessage = "IntervalAuthPolicyUpdaterMinutes must be greater than zero.")]
-    public int IntervalAuthPolicyUpdaterMinutes { get; set; }
-}
