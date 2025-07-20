0eaa721c51ed4bc6e079d30d7ebfabf015e54a12
Angelo Pirola <angelo9871@gmail.com>
2025-07-21 00:05:23 +0200

    Remove AuthPolicy constant from CustomClaimTypes
    This commit removes the `AuthPolicy` constant from the
    `CustomClaimTypes` class, while keeping the existing
    constants `FullName` and `Module` intact.

--- src/MinimalApi.Identity.API/Constants/CustomClaimTypes.cs
+++ src/MinimalApi.Identity.API/Constants/CustomClaimTypes.cs
@@ -4,7 +4,6 @@ namespace MinimalApi.Identity.API.Constants;
 
 public static class CustomClaimTypes
 {
-    //public const string AuthPolicy = nameof(ClaimsType.AuthPolicy);
     public const string FullName = nameof(ClaimsType.FullName);
     public const string Module = nameof(ClaimsType.Module);
 }
