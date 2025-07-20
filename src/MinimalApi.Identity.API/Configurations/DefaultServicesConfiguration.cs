d1dc9e4c6834fdcc939d4f4309a8a525634787fa
Angelo Pirola <angelo9871@gmail.com>
2025-07-21 00:04:12 +0200

    Update DefaultServicesConfiguration constructor
    Modified the constructor to accept an IServiceCollection parameter and added a private readonly field to store it. Clarified comments for the Configure and DatabaseConnectionString properties, emphasizing the need for initialization before use. Initialized FormatErrorResponse to a default value of ErrorResponseFormat.Default.

--- src/MinimalApi.Identity.API/Configurations/DefaultServicesConfiguration.cs
+++ src/MinimalApi.Identity.API/Configurations/DefaultServicesConfiguration.cs
@@ -9,6 +9,5 @@ public class DefaultServicesConfiguration(IServiceCollection services)
     private readonly IServiceCollection services = services;
 
     public IConfiguration Configure { get; set; } = null!; // Must be set before using
-    public string DatabaseConnectionString { get; set; } = null!; // Must be set before using
     public ErrorResponseFormat FormatErrorResponse { get; set; } = ErrorResponseFormat.Default; // Default format for error responses
 }
