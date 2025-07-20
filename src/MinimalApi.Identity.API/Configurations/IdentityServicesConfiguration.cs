3ecf24c43dfbf531e9ae4a6320db78886efe21d0
Angelo Pirola <angelo9871@gmail.com>
2025-07-20 09:15:15 +0200

    Add IdentityServicesConfiguration class
    Introduces the `IdentityServicesConfiguration` class in the
    `MinimalApi.Identity.API.Configurations` namespace. This class
    accepts an `IServiceCollection` in its constructor and includes
    properties for `JWTOptions` and `IdentityOptions`, which must
    be set before use. Also adds the necessary using directive
    for `Microsoft.Extensions.DependencyInjection`.

---
+++ src/MinimalApi.Identity.API/Configurations/IdentityServicesConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MinimalApi.Identity.API.Options;
+
+namespace MinimalApi.Identity.API.Configurations;
+
+public class IdentityServicesConfiguration(IServiceCollection services)
+{
+    private readonly IServiceCollection services = services;
+
+    public JwtOptions JWTOptions { get; set; } = null!; // Must be set before using
+    public NetIdentityOptions IdentityOptions { get; set; } = null!; // Must be set before using
+}
