ad1e3f959b8b112fcf02b7521c36e19bc95160ad
Angelo Pirola <angelo9871@gmail.com>
2025-07-22 08:50:32 +0200

    Add ConstantsConfiguration class for application constants
    Introduced a new static class `ConstantsConfiguration` in the
    `MinimalApi.Identity.Core.Configurations` namespace. This class
    includes public static string properties for various constant
    values, such as URLs and error messages, to enhance consistency
    and maintainability across the application.

---
+++ src/MinimalApi.Identity.Core/Configurations/ConstantsConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace MinimalApi.Identity.Core.Configurations;
+
+public static class ConstantsConfiguration
+{
+    public static string WebSiteDev => "https://angelo.aepserver.it/";
+    public static string LicenseMIT => "https://opensource.org/licenses/MIT";
+    public static string NoActivePoliciesFound => "No active policies found in the database.";
+    public static string NotFound => "Not Found";
+    public static string BadRequest => "Bad Request";
+    public static string Unauthorized => "Unauthorized";
+}
