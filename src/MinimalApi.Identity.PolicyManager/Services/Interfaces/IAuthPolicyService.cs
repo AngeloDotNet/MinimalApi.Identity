030b63b19f5ba877a5af851a971192b2015f6f54
Angelo Pirola <angelo9871@gmail.com>
2025-07-23 08:50:21 +0200

    Add CancellationToken support to IAuthPolicyService
    Updated IAuthPolicyService methods to include CancellationToken parameters for GetAllPoliciesAsync, CreatePolicyAsync, and DeletePolicyAsync. This enhancement allows for better handling of cancellation requests in asynchronous operations.

--- src/MinimalApi.Identity.PolicyManager/Services/Interfaces/IAuthPolicyService.cs
+++ src/MinimalApi.Identity.PolicyManager/Services/Interfaces/IAuthPolicyService.cs
@@ -6,9 +6,9 @@ namespace MinimalApi.Identity.PolicyManager.Services.Interfaces;
 
 public interface IAuthPolicyService
 {
-    Task<List<PolicyResponseModel>> GetAllPoliciesAsync();
-    Task<string> CreatePolicyAsync(CreatePolicyModel model);
-    Task<string> DeletePolicyAsync(DeletePolicyModel model);
+    Task<List<PolicyResponseModel>> GetAllPoliciesAsync(CancellationToken cancellationToken);
+    Task<string> CreatePolicyAsync(CreatePolicyModel model, CancellationToken cancellationToken);
+    Task<string> DeletePolicyAsync(DeletePolicyModel model, CancellationToken cancellationToken);
     Task<bool> GenerateAuthPoliciesAsync();
     Task<bool> UpdateAuthPoliciesAsync();
     Task<List<AuthPolicy>> GetAllAuthPoliciesAsync(Expression<Func<AuthPolicy, bool>> filter = null!);
