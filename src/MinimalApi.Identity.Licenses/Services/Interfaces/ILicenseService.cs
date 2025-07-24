437aa6f7ff33273031c9ed07616401e807456d3b
Angelo Pirola <angelo9871@gmail.com>
2025-07-24 08:47:01 +0200

    Update ILicenseService to support cancellation token
    Modified the `GetAllLicensesAsync` method signature in the
    `ILicenseService` interface to include a `CancellationToken`
    parameter, enhancing its usability in asynchronous operations.

--- src/MinimalApi.Identity.Licenses/Services/Interfaces/ILicenseService.cs
+++ src/MinimalApi.Identity.Licenses/Services/Interfaces/ILicenseService.cs
@@ -6,7 +6,7 @@ namespace MinimalApi.Identity.Licenses.Services.Interfaces;
 
 public interface ILicenseService
 {
-    Task<List<LicenseResponseModel>> GetAllLicensesAsync();
+    Task<List<LicenseResponseModel>> GetAllLicensesAsync(CancellationToken cancellationToken);
     Task<string> CreateLicenseAsync(CreateLicenseModel model, CancellationToken cancellationToken);
     Task<string> AssignLicenseAsync(AssignLicenseModel model, CancellationToken cancellationToken);
     Task<string> RevokeLicenseAsync(RevokeLicenseModel model, CancellationToken cancellationToken);
