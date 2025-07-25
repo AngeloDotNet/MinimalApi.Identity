26d32e10c0107d29710ea6f84c6db6a334d81184
Angelo Pirola <angelo9871@gmail.com>
2025-07-26 00:01:29 +0200

    Add AuthOptions class for configuration management
    Introduces the `AuthOptions` class in the `MinimalApi.Identity.API.Options` namespace. This class encapsulates configuration settings by accepting `IOptions<JwtOptions>`, `IOptions<NetIdentityOptions>`, and `IOptions<UsersOptions>` as constructor parameters, and initializes properties for easy access to these options.

---
+++ src/MinimalApi.Identity.API/Options/AuthOptions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace MinimalApi.Identity.API.Options;
+
+public class AuthOptions(IOptions<JwtOptions> jOptions, IOptions<NetIdentityOptions> iOptions, IOptions<UsersOptions> uOptions)
+{
+    public JwtOptions Jwt { get; } = jOptions.Value;
+    public NetIdentityOptions NetIdentity { get; } = iOptions.Value;
+    public UsersOptions Users { get; } = uOptions.Value;
+}
