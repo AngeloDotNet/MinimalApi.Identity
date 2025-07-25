76b8959c87d86d24ae291ecbad255993fc69af2b
Angelo Pirola <angelo9871@gmail.com>
2025-07-26 00:02:20 +0200

    Add PolicyMapper for entity and model mapping
    Introduce a new static class `PolicyMapper` in the
    `MinimalApi.Identity.PolicyManager.Mapping` namespace.
    This class provides extension methods to map
    `AuthPolicy` entities to `PolicyResponseModel` and
    `PolicyDetailsResponseModel`, and vice versa.
    These enhancements improve data transformation
    capabilities across application layers.

---
+++ src/MinimalApi.Identity.PolicyManager/Mapping/PolicyMapper.cs
@@ -0,0 +1,25 @@
+﻿using MinimalApi.Identity.Core.Entities;
+using MinimalApi.Identity.PolicyManager.Models;
+
+namespace MinimalApi.Identity.PolicyManager.Mapping;
+
+public static class PolicyMapper
+{
+    public static IQueryable<PolicyResponseModel> ToModel(this IQueryable<AuthPolicy> query)
+        => query.Select(p => new PolicyResponseModel(p.Id, p.PolicyName, p.PolicyDescription, p.PolicyPermissions));
+
+    public static IQueryable<PolicyDetailsResponseModel> ToDetailsModel(this IQueryable<AuthPolicy> query)
+        => query.Select(p => new PolicyDetailsResponseModel(p.Id, p.PolicyName, p.PolicyDescription, p.PolicyPermissions, p.IsDefault, p.IsActive));
+
+    public static AuthPolicy ToEntityModel(this PolicyDetailsResponseModel model)
+    {
+        return new AuthPolicy
+        {
+            PolicyName = model.PolicyName,
+            PolicyDescription = model.PolicyDescription,
+            PolicyPermissions = model.PolicyPermissions,
+            IsDefault = model.IsDefault,
+            IsActive = model.IsActive
+        };
+    }
+}
