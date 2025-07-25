bf48496b4d5e5ce21fe43abf5c1bb4d92c05d48a
Angelo Pirola <angelo9871@gmail.com>
2025-07-26 00:02:37 +0200

    Add PolicyDetailsResponseModel record class
    Introduced a new record class `PolicyDetailsResponseModel` in the `MinimalApi.Identity.PolicyManager.Models` namespace. This class encapsulates properties such as `Id`, `PolicyName`, `PolicyDescription`, `PolicyPermissions`, `IsDefault`, and `IsActive` to represent policy details in the application.

---
+++ src/MinimalApi.Identity.PolicyManager/Models/PolicyDetailsResponseModel.cs
@@ -0,0 +1,3 @@
+﻿namespace MinimalApi.Identity.PolicyManager.Models;
+
+public record class PolicyDetailsResponseModel(int Id, string PolicyName, string PolicyDescription, string[] PolicyPermissions, bool IsDefault, bool IsActive);
