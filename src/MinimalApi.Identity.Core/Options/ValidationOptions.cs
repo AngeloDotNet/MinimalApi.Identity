dc7b6f70efae399310e54a5d611660a534e0d190
Angelo Pirola <angelo9871@gmail.com>
2025-07-27 00:03:39 +0200

    Add length validation properties to ValidationOptions
    Introduced four new properties: MinLength, MaxLength,
    MinLengthDescription, and MaxLengthDescription in the
    ValidationOptions class. All properties are of type int
    and utilize the init accessor for immutability.

--- src/MinimalApi.Identity.Core/Options/ValidationOptions.cs
+++ src/MinimalApi.Identity.Core/Options/ValidationOptions.cs
@@ -4,5 +4,9 @@ namespace MinimalApi.Identity.Core.Options;
 
 public class ValidationOptions
 {
+    public int MinLength { get; init; }
+    public int MaxLength { get; init; }
+    public int MinLengthDescription { get; init; }
+    public int MaxLengthDescription { get; init; }
     public ErrorResponseFormat ErrorResponseFormat { get; set; }
 }
