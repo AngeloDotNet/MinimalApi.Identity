feab87d3997bbc5a7bd759e9827cfbe57c6ada45
Angelo Pirola <angelo9871@gmail.com>
2025-07-30 00:17:56 +0200

    Add EndpointsHandler for policy management operations
    Introduces a new static class `EndpointsHandler` in the
    `MinimalApi.Identity.PolicyManager.Extensions` namespace.
    This class includes three asynchronous methods:
    `GetAllHandlerAsync`, `CreateHandlerAsync`, and
    `DeleteHandlerAsync`, which utilize the `IAuthPolicyService`
    to manage policy-related operations. Each method returns
    results wrapped in an `IResult` type, indicating success
    with `Results.Ok()`.

---
+++ src/MinimalApi.Identity.PolicyManager/Extensions/EndpointsHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MinimalApi.Identity.PolicyManager.Models;
+using MinimalApi.Identity.PolicyManager.Services.Interfaces;
+
+namespace MinimalApi.Identity.PolicyManager.Extensions;
+
+public static class EndpointsHandler
+{
+    public static async Task<IResult> GetAllHandlerAsync([FromServices] IAuthPolicyService authPolicyService, HttpContext httpContext)
+    {
+        var result = await authPolicyService.GetAllPoliciesAsync(httpContext.RequestAborted);
+        return Results.Ok(result);
+    }
+
+    public static async Task<IResult> CreateHandlerAsync([FromServices] IAuthPolicyService authPolicyService,
+        [FromBody] CreatePolicyModel inputModel, HttpContext httpContext)
+    {
+        var result = await authPolicyService.CreatePolicyAsync(inputModel, httpContext.RequestAborted);
+        return Results.Ok(result);
+    }
+
+    public static async Task<IResult> DeleteHandlerAsync([FromServices] IAuthPolicyService authPolicyService,
+        [FromBody] DeletePolicyModel inputModel, HttpContext httpContext)
+    {
+        var result = await authPolicyService.DeletePolicyAsync(inputModel, httpContext.RequestAborted);
+        return Results.Ok(result);
+    }
+}
