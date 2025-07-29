e8b7f8495f5974aad60769e1f43aefbfe9dd9694
Angelo Pirola <angelo9871@gmail.com>
2025-07-30 01:21:20 +0200

    Refactor OnModelCreating in MinimalApiAuthDbContext
    Updated the `OnModelCreating` method to include a conditional check that registers only non-abstract classes implementing the `IEntity` interface. Removed outdated code and improved clarity in entity registration logic.

--- src/MinimalApi.Identity.Core/Database/MinimalApiAuthDbContext.cs
+++ src/MinimalApi.Identity.Core/Database/MinimalApiAuthDbContext.cs
@@ -13,14 +13,26 @@ public class MinimalApiAuthDbContext(DbContextOptions<MinimalApiAuthDbContext> o
 {
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        //base.OnModelCreating(builder);
+
+        //var entityTypes = Assembly.GetExecutingAssembly()
+        //    .GetTypes().Where(t => t.IsClass && !t.IsAbstract && typeof(IEntity).IsAssignableFrom(t));
+
+        //foreach (var type in entityTypes)
+        //{
+        //    builder.Entity(type);
+        //}
+
         base.OnModelCreating(builder);
 
-        var entityTypes = Assembly.GetExecutingAssembly()
-            .GetTypes().Where(t => t.IsClass && !t.IsAbstract && typeof(IEntity).IsAssignableFrom(t));
+        var assembly = Assembly.GetExecutingAssembly();
 
-        foreach (var type in entityTypes)
+        foreach (var type in assembly.GetTypes())
         {
-            builder.Entity(type);
+            if (type.IsClass && !type.IsAbstract && typeof(IEntity).IsAssignableFrom(type))
+            {
+                builder.Entity(type);
+            }
         }
 
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
