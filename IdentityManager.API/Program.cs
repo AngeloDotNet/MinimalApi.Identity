301684da5d9ef154f10d4683d5c5ac19f721ba82
Angelo Pirola <angelo9871@gmail.com>
2025-08-02 10:56:03 +0200

    Refactor Main method and service registration
    - Changed Main method to asynchronous to support await.
    - Introduced configuration variable for application settings.
    - Modified AddRegisterDefaultServices to retrieve MigrationsAssembly from configuration.
    - Added a new service registration block for interfaces and services with Transient lifetime.
    - Integrated LicenseRegistrationService into the new service registration block.
    - Updated ConfigureDatabaseAsync call to use RegisterServicesExtensions.
    - Overall improvements to service registration and configuration handling.

--- IdentityManager.API/Program.cs
+++ IdentityManager.API/Program.cs
@@ -1,6 +1,8 @@
 using MinimalApi.Identity.API.Extensions;
 using MinimalApi.Identity.API.Middleware;
+using MinimalApi.Identity.API.Services.Interfaces;
 using MinimalApi.Identity.Core.Database;
+using MinimalApi.Identity.Core.DependencyInjection;
 using MinimalApi.Identity.Core.Enums;
 using MinimalApi.Identity.Licenses.DependencyInjection;
 using MinimalApi.Identity.Licenses.Endpoints;
@@ -9,27 +11,51 @@ namespace IdentityManager.API;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        var configuration = builder.Configuration;
 
         builder.Services.AddCors(options => options.AddPolicy("cors", builder
             => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 
-        builder.Services.AddRegisterDefaultServices<MinimalApiAuthDbContext, Program>(options =>
-        {
-            options.Configure = builder.Configuration;
-            options.FormatErrorResponse = ErrorResponseFormat.List; // or ErrorResponseFormat.Default
-        });
+        var migrationsAssembly = builder.Configuration.GetValue<string>("ApplicationOptions:MigrationsAssembly");
 
-        builder.Services.LicenseRegistrationService(); //Register licensing services if this feature is needed, otherwise just comment out this line.
+        if (string.IsNullOrEmpty(migrationsAssembly))
+        {
+            migrationsAssembly = typeof(Program).Assembly.FullName!;
+        }
+
+        builder.Services
+            // If you need to register services with a lifecycle other than Transient, do not modify this configuration.
+            .AddRegisterServices(options =>
+            {
+                options.Interfaces = [typeof(IAuthService)]; // Register your interfaces here, but do not remove the IAuthService service.
+                options.StringEndsWith = "Service"; // This will register all services that end with "Service" in the assembly.
+                options.Lifetime = ServiceLifetime.Transient; // This will register the services with a Transient lifetime.
+            })
+            // Instead, create one (or more) duplicates like the one below, modifying it as needed.
+            // The life cycle can take the values: Transient, Singleton and Scoped
+            //.AddRegisterServices(options =>
+            //{
+            //    options.Interfaces = [typeof(YourServiceRepository)]; // Replace YourServiceRepository with your actual service interfaces.
+            //    options.StringEndsWith = "Repository"; // This will register all services that end with "Repository" in the assembly.
+            //    options.Lifetime = ServiceLifetime.Scoped; // This will register the services with a Scoped lifetime.
+            //})
+            .LicenseRegistrationService() //Register licensing services if this feature is needed, otherwise just comment out this line.
+            .AddRegisterDefaultServices<MinimalApiAuthDbContext>(options =>
+            {
+                options.MigrationsAssembly = migrationsAssembly;
+                options.FormatErrorResponse = ErrorResponseFormat.List; // or ErrorResponseFormat.Default
+            }, configuration);
+
         builder.Services.AddAuthorization(options =>
         {
             // Here you can add additional authorization policies
         });
 
         var app = builder.Build();
-        //await ConfigureDatabaseAsync(app.Services);
+        await RegisterServicesExtensions.ConfigureDatabaseAsync(app.Services);
 
         app.UseHttpsRedirection();
         app.UseStatusCodePages();
