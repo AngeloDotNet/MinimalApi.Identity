a1dcfd0a49be1ae15b5d54dd3c93cc58358137f5
Angelo Pirola <angelo9871@gmail.com>
2025-08-04 00:10:13 +0200

    Add ManageOptionsExtensions for feature flag management
    Introduces a new static class `ManageOptionsExtensions` in the `MinimalApi.Identity.API.Extensions` namespace. This class includes a private static field `featureFlagsOptions` and a public static method `GetFeatureFlagsOptions` to manage feature flag configurations. The method retrieves options from `IConfiguration`, initializes defaults if necessary, and updates existing values based on the configuration.

---
+++ src/MinimalApi.Identity.API/Extensions/ManageOptionsExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using MinimalApi.Identity.API.Options;
+
+namespace MinimalApi.Identity.API.Extensions;
+
+public static class ManageOptionsExtensions
+{
+    private static FeatureFlagsOptions featureFlagsOptions;
+
+    //TODO: Uncomment and implement JwtOptions if needed
+    //private static JwtOptions jwtOptions;
+
+    // TODO: Uncomment and implement ValidationOptions if needed
+    //private static ValidationOptions validationOptions;
+
+    public static FeatureFlagsOptions GetFeatureFlagsOptions(IConfiguration configuration)
+    {
+        var configOptions = configuration.GetSection("FeatureFlagsOptions").Get<FeatureFlagsOptions>();
+
+        if (featureFlagsOptions == null)
+        {
+            featureFlagsOptions = configOptions ?? new FeatureFlagsOptions
+            {
+                EnabledFeatureLicense = false,
+                EnabledFeatureModule = false
+            };
+        }
+        else if (configOptions != null)
+        {
+            featureFlagsOptions.EnabledFeatureLicense = configOptions.EnabledFeatureLicense;
+            featureFlagsOptions.EnabledFeatureModule = configOptions.EnabledFeatureModule;
+        }
+
+        return featureFlagsOptions;
+    }
+}
