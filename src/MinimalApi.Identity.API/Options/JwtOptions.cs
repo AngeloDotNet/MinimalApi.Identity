0e84f0f85683877b5c3c45bf2c1b92f25b960025
Angelo Pirola <angelo9871@gmail.com>
2025-08-06 00:04:38 +0200

    Refactor JwtOptions class for read-write properties
    Updated the JwtOptions class to change properties from read-only (`init`) to read-write (`set`). Introduced a new `SchemaName` property and added a `ClockSkew` property with a default value. Removed the `using` directive for `System.ComponentModel.DataAnnotations` and added `Microsoft.IdentityModel.Tokens`.

--- src/MinimalApi.Identity.API/Options/JwtOptions.cs
+++ src/MinimalApi.Identity.API/Options/JwtOptions.cs
@@ -1,55 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
-using MinimalApi.Identity.Core.Validator.Attribute;
+﻿using Microsoft.IdentityModel.Tokens;
 
 namespace MinimalApi.Identity.API.Options;
 
 public class JwtOptions
 {
-    [Required, MinLength(1, ErrorMessage = "Issuer cannot be empty.")]
-    public string Issuer { get; init; } = null!;
+    public string SchemaName { get; set; } = null!;
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+    public string SecurityKey { get; set; } = null!;
+    public TimeSpan ClockSkew { get; set; } = TokenValidationParameters.DefaultClockSkew;
 
-    [Required, MinLength(1, ErrorMessage = "Audience cannot be empty.")]
-    public string Audience { get; init; } = null!;
-
-    [Required, MinLength(1, ErrorMessage = "SecurityKey cannot be empty.")]
-    public string SecurityKey { get; init; } = null!;
-
-    [Required, Range(1, int.MaxValue, ErrorMessage = "AccessTokenExpirationMinutes must be greater than zero.")]
-    public int AccessTokenExpirationMinutes { get; init; }
-
-    [Required, Range(1, int.MaxValue, ErrorMessage = "RefreshTokenExpirationMinutes must be greater than zero.")]
-    public int RefreshTokenExpirationMinutes { get; init; }
-
-    [Required]
-    public bool RequireUniqueEmail { get; init; }
-
-    [Required]
-    public bool RequireDigit { get; init; }
-
-    [Required, Range(1, int.MaxValue, ErrorMessage = "RequiredLength must be greater than zero.")]
-    public int RequiredLength { get; init; }
-
-    [Required]
-    public bool RequireUppercase { get; init; }
-
-    [Required]
-    public bool RequireLowercase { get; init; }
-
-    [Required]
-    public bool RequireNonAlphanumeric { get; init; }
-
-    [Required, Range(1, int.MaxValue, ErrorMessage = "RequiredUniqueChars must be greater than zero.")]
-    public int RequiredUniqueChars { get; init; }
-
-    [Required]
-    public bool RequireConfirmedEmail { get; init; }
-
-    [Required, Range(1, int.MaxValue, ErrorMessage = "MaxFailedAccessAttempts must be greater than zero.")]
-    public int MaxFailedAccessAttempts { get; init; }
-
-    [Required]
-    public bool AllowedForNewUsers { get; init; }
-
-    [Required, TimeSpanRange("00:00:01", "1.00:00:00", ErrorMessage = "DefaultLockoutTimeSpan must be between 1 second and 1 day.")]
-    public TimeSpan DefaultLockoutTimeSpan { get; init; }
+    public int AccessTokenExpirationMinutes { get; set; }
+    public int RefreshTokenExpirationMinutes { get; set; }
+    public bool RequireUniqueEmail { get; set; }
+    public bool RequireDigit { get; set; }
+    public int RequiredLength { get; set; }
+    public bool RequireUppercase { get; set; }
+    public bool RequireLowercase { get; set; }
+    public bool RequireNonAlphanumeric { get; set; }
+    public int RequiredUniqueChars { get; set; }
+    public bool RequireConfirmedEmail { get; set; }
+    public int MaxFailedAccessAttempts { get; set; }
+    public bool AllowedForNewUsers { get; set; }
+    public TimeSpan DefaultLockoutTimeSpan { get; set; }
 }
