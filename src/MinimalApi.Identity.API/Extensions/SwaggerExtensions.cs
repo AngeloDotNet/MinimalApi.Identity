f024898d2a7a7290b45f8efbe7f104bc018ba29b
Angelo Pirola <angelo9871@gmail.com>
2025-08-06 00:06:22 +0200

    Add SwaggerExtensions for OpenAPI configuration
    Introduces a new static class `SwaggerExtensions` with methods to:
    - Configure OpenAPI metadata with `AddOpenApiInfo`.
    - Set up Bearer token authentication with `AddOpenApiSecuritySchemeRequirement`.
    - Apply document filters based on feature flags with `AddSwaggerDocumentFilters`.
    
    Also adds necessary using directives for authentication and OpenAPI models.

---
+++ src/MinimalApi.Identity.API/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+using MinimalApi.Identity.API.Options;
+using MinimalApi.Identity.API.Swagger;
+using MinimalApi.Identity.Core.Configurations;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace MinimalApi.Identity.API.Extensions;
+
+public static class SwaggerExtensions
+{
+    public static void AddOpenApiInfo(this SwaggerGenOptions options)
+    {
+        var openApiInfo = new OpenApiInfo
+        {
+            Title = "Minimal API Identity",
+            Version = "v1",
+            Contact = new OpenApiContact
+            {
+                Name = "Angelo Pirola",
+                Email = "angelo@aepserver.it",
+                Url = new Uri(ConstantsConfiguration.WebSiteDev)
+            },
+            License = new OpenApiLicense
+            {
+                Name = "License MIT",
+                Url = new Uri(ConstantsConfiguration.LicenseMIT)
+            },
+        };
+        options.SwaggerDoc("v1", openApiInfo);
+    }
+
+    public static void AddOpenApiSecuritySchemeRequirement(this SwaggerGenOptions options)
+    {
+        var securityScheme = new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Insert the Bearer Token",
+            Name = HeaderNames.Authorization,
+            Type = SecuritySchemeType.ApiKey,
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = JwtBearerDefaults.AuthenticationScheme
+            }
+        };
+        options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
+
+        var securityRequirement = new OpenApiSecurityRequirement
+        {
+            { securityScheme, Array.Empty<string>() }
+        };
+        options.AddSecurityRequirement(securityRequirement);
+    }
+
+    public static void AddSwaggerDocumentFilters(this SwaggerGenOptions options, FeatureFlagsOptions featureFlagsOptions)
+    {
+        options.DocumentFilter<SwaggerLicenseDocumentFilter>(featureFlagsOptions);
+        options.DocumentFilter<SwaggerModulesDocumentFilter>(featureFlagsOptions);
+    }
+}
