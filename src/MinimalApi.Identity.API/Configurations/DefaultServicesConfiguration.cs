5ae166fa3f3666d2a22b2b7cb80585885990070b
Angelo Pirola <angelo9871@gmail.com>
2025-08-06 00:03:31 +0200

    Add configuration properties for JWT and feature flags
    This commit introduces new using directives and adds several properties to the DefaultServicesConfiguration class:
    - `DatabaseType` and `MigrationsAssembly` for database configuration.
    - `JwtOptions` for JWT options configuration.
    - `FeatureFlags` for feature flags configuration.
    The existing `FormatErrorResponse` property remains unchanged.

--- src/MinimalApi.Identity.API/Configurations/DefaultServicesConfiguration.cs
+++ src/MinimalApi.Identity.API/Configurations/DefaultServicesConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using MinimalApi.Identity.API.Options;
 using MinimalApi.Identity.Core.Enums;
 
 namespace MinimalApi.Identity.API.Configurations;
@@ -7,6 +8,9 @@ public class DefaultServicesConfiguration(IServiceCollection services)
 {
     private readonly IServiceCollection services = services;
 
+    public string DatabaseType { get; set; } = string.Empty; // Must be set before using
     public string MigrationsAssembly { get; set; } = string.Empty; // Must be set before using
+    public JwtOptions JwtOptions { get; set; } = new JwtOptions(); // JWT options configuration, if needed
+    public FeatureFlagsOptions FeatureFlags { get; set; } = new FeatureFlagsOptions(); // Feature flags configuration
     public ErrorResponseFormat FormatErrorResponse { get; set; } = ErrorResponseFormat.Default; // Default format for error responses
 }
