47ac553b06a08d1cb533e12303cdb7e281795a87
Angelo Pirola <angelo9871@gmail.com>
2025-08-06 00:06:14 +0200

    Refactor service registration in RegisterServicesExtensions
    Reordered parameters in method signatures for clarity and streamlined service configuration.
    Updated `AddRegisterDefaultServices` and `AddDatabaseContext` methods to simplify configuration processes.
    Replaced action delegate with `JwtOptions` in `AddMinimalApiIdentityServices` for easier JWT settings configuration.
    Removed `AddOptionsConfiguration` and integrated its functionality into `AddRegisterDefaultServices`.
    Added comments for clarity and retained some commented-out code for future use.
    These changes improve the readability, maintainability, and configurability of the service registration process.

--- src/MinimalApi.Identity.API/Extensions/RegisterServicesExtensions.cs
+++ src/MinimalApi.Identity.API/Extensions/RegisterServicesExtensions.cs
@@ -14,14 +14,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.Net.Http.Headers;
-using Microsoft.OpenApi.Models;
 using MinimalApi.Identity.API.Configurations;
 using MinimalApi.Identity.API.Options;
-using MinimalApi.Identity.API.Swagger;
 using MinimalApi.Identity.API.Validator;
 using MinimalApi.Identity.Core.Authorization;
-using MinimalApi.Identity.Core.Configurations;
 using MinimalApi.Identity.Core.Database;
 using MinimalApi.Identity.Core.DependencyInjection;
 using MinimalApi.Identity.Core.Entities;
@@ -37,33 +33,18 @@ namespace MinimalApi.Identity.API.Extensions;
 
 public static class RegisterServicesExtensions
 {
-    public static IServiceCollection AddRegisterDefaultServices<TDbContext>(this IServiceCollection services,
-        Action<DefaultServicesConfiguration> configure, IConfiguration configuration) where TDbContext : DbContext
+    public static IServiceCollection AddRegisterDefaultServices<TDbContext>(this IServiceCollection services, IConfiguration configuration,
+        Action<DefaultServicesConfiguration> configure) where TDbContext : DbContext
     {
         var settings = new DefaultServicesConfiguration(services);
         configure(settings);
-
-        var hostedServiceOptions = configuration.GetSection(nameof(HostedServiceOptions)).Get<HostedServiceOptions>();
-        var jwtOptions = configuration.GetSection(nameof(JwtOptions)).Get<JwtOptions>();
-        var smtpOptions = configuration.GetSection(nameof(SmtpOptions)).Get<SmtpOptions>();
-        var userOptions = configuration.GetSection(nameof(UsersOptions)).Get<UsersOptions>();
 
         services
             .AddProblemDetails()
             .AddHttpContextAccessor()
             .AddSwaggerConfiguration(settings.FeatureFlags)
-            .AddDatabaseContext<TDbContext>(options =>
-            {
-                options.MigrationsAssembly = settings.MigrationsAssembly;
-                options.DatabaseType = configuration.GetSection("ConnectionStrings").GetValue<string>("DatabaseType")!;
-            }, configuration)
-            .AddMinimalApiIdentityServices<TDbContext, ApplicationUser>(options =>
-            {
-                options.JWTOptions = jwtOptions ?? new();
-            })
-            .AddOptionsConfiguration(configuration)
-            .ConfigureValidation(options => options.ErrorResponseFormat = settings.FormatErrorResponse)
-            .ConfigureFluentValidation<LoginValidator>();
+            .AddDatabaseContext<TDbContext>(configuration, settings.DatabaseType, settings.MigrationsAssembly)
+            .AddMinimalApiIdentityServices<TDbContext, ApplicationUser>(settings.JwtOptions);
 
         services
             .AddSingleton<IHostedService, AuthorizationPolicyGeneration>()
@@ -71,14 +52,50 @@ public static class RegisterServicesExtensions
             .AddScoped<IAuthorizationHandler, PermissionHandler>()
             .AddHostedService<AuthorizationPolicyUpdater>();
 
+        services
+            //.Configure<JsonOptions>(options =>
+            //{
+            //    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            //    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+            //    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+            //    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            //    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+            //    options.JsonSerializerOptions.WriteIndented = true;
+            //})
+            .Configure<JsonOptions>(options => options.ConfigureJsonOptions())
+            .Configure<HostedServiceOptions>(configuration.GetSection(nameof(HostedServiceOptions)))
+            .Configure<JwtOptions>(configuration.GetSection(nameof(JwtOptions)))
+            .Configure<SmtpOptions>(configuration.GetSection(nameof(SmtpOptions)))
+            .Configure<UsersOptions>(configuration.GetSection(nameof(UsersOptions)))
+            .Configure<ValidationOptions>(configuration.GetSection(nameof(ValidationOptions)))
+            .Configure<RouteOptions>(options => options.LowercaseUrls = true)
+            .Configure<KestrelServerOptions>(configuration.GetSection("Kestrel"))
+
+            .ConfigureValidation(options => options.ErrorResponseFormat = settings.FormatErrorResponse)
+            .ConfigureFluentValidation<LoginValidator>();
+
         return services;
+    }
+
+    private static JsonOptions ConfigureJsonOptions(this JsonOptions jsonOptions)
+    {
+        var options = new JsonOptions();
+
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+        options.JsonSerializerOptions.WriteIndented = true;
+
+        return options;
     }
 
     public static IServiceCollection AddModulesRegistrations(this IServiceCollection services, FeatureFlagsOptions featureFlagsOptions)
     {
-        services.PolicyManagerRegistrationService(); // Register PolicyManager package services
-                                                     //.AccountManagerRegistrationService() // Register AccountManager package services
-                                                     //.EmailManagerRegistrationService() // Register EmailManager package services
+        services.PolicyManagerRegistrationService();
+        //.AccountManagerRegistrationService()
+        //.EmailManagerRegistrationService()
 
         if (featureFlagsOptions.EnabledFeatureLicense)
         {
@@ -91,9 +108,9 @@ public static class RegisterServicesExtensions
     public static void UseMapEndpoints(this WebApplication app, FeatureFlagsOptions featureFlagsOptions)
     {
         app.MapEndpoints();
-        //app.MapAccountEndpoints(); //Register AccountManager package endpoints
-        //app.MapEmailEndpoints(); //Register EmailManager package endpoints
-        app.MapPolicyEndpoints(); //Register PolicyManager package endpoints
+        //app.MapAccountEndpoints();
+        //app.MapEmailEndpoints(); 
+        app.MapPolicyEndpoints();
 
         if (featureFlagsOptions.EnabledFeatureLicense)
         {
@@ -107,66 +124,25 @@ public static class RegisterServicesExtensions
             .AddEndpointsApiExplorer()
             .AddSwaggerGen(options =>
             {
-                var openApiInfo = new OpenApiInfo
-                {
-                    Title = "Minimal API Identity",
-                    Version = "v1",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Angelo Pirola",
-                        Email = "angelo@aepserver.it",
-                        Url = new Uri(ConstantsConfiguration.WebSiteDev)
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "License MIT",
-                        Url = new Uri(ConstantsConfiguration.LicenseMIT)
-                    },
-                };
-                options.SwaggerDoc("v1", openApiInfo);
-
-                var securityScheme = new OpenApiSecurityScheme
-                {
-                    In = ParameterLocation.Header,
-                    Description = "Insert the Bearer Token",
-                    Name = HeaderNames.Authorization,
-                    Type = SecuritySchemeType.ApiKey,
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = JwtBearerDefaults.AuthenticationScheme
-                    }
-                };
-                options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
-
-                var securityRequirement = new OpenApiSecurityRequirement
-                {
-                    { securityScheme, Array.Empty<string>() }
-                };
-                options.AddSecurityRequirement(securityRequirement);
-
-                // Register SwaggerModule DocumentFilter to conditionally remove inactive endpoints
-                options.DocumentFilter<SwaggerModulesDocumentFilter>(featureFlagsOptions);
+                options.AddOpenApiInfo();
+                options.AddOpenApiSecuritySchemeRequirement();
+                options.AddSwaggerDocumentFilters(featureFlagsOptions);
             });
     }
 
-    public static IServiceCollection AddDatabaseContext<TDbContext>(this IServiceCollection services, Action<DatabaseServiceConfiguration> configure,
-        IConfiguration configuration) where TDbContext : DbContext
+    public static IServiceCollection AddDatabaseContext<TDbContext>(this IServiceCollection services, IConfiguration configuration,
+        string databaseType, string migrationAssembly) where TDbContext : DbContext
     {
-        var settings = new DatabaseServiceConfiguration(services);
-        configure.Invoke(settings);
-
-        if (settings.DatabaseType.Equals("sqlserver", StringComparison.InvariantCultureIgnoreCase))
+        if (databaseType.Equals("sqlserver", StringComparison.InvariantCultureIgnoreCase))
         {
             var sqlConnection = configuration.GetConnectionString("SQLServer");
 
-            services.AddDbContext<TDbContext>(options
-                => options.UseSqlServer(sqlConnection, opt =>
-                {
-                    opt.MigrationsAssembly(settings.MigrationsAssembly);
-                    opt.MigrationsHistoryTable(HistoryRepository.DefaultTableName);
-                    opt.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
-                }));
+            services.AddDbContext<TDbContext>(options => options.UseSqlServer(sqlConnection, opt =>
+            {
+                opt.MigrationsAssembly(migrationAssembly);
+                opt.MigrationsHistoryTable(HistoryRepository.DefaultTableName);
+                opt.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+            }));
         }
 
         return services;
@@ -181,11 +157,8 @@ public static class RegisterServicesExtensions
     }
 
     internal static IServiceCollection AddMinimalApiIdentityServices<TDbContext, TEntityUser>(this IServiceCollection services,
-        Action<IdentityServicesConfiguration> configure) where TDbContext : DbContext where TEntityUser : class
+        JwtOptions settings) where TDbContext : DbContext where TEntityUser : class
     {
-        var configuration = new IdentityServicesConfiguration(services);
-        configure.Invoke(configuration);
-
         services
             .AddIdentity<TEntityUser, ApplicationRole>()
             .AddEntityFrameworkStores<TDbContext>()
@@ -193,65 +166,54 @@ public static class RegisterServicesExtensions
 
         services
             .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
+            .AddJwtBearer(settings.SchemaName, options =>
             {
                 options.SaveToken = true;
                 options.RequireHttpsMetadata = false;
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
+                    //ValidateIssuer = true,
+                    //ValidIssuer = settings.Issuer,
+                    //ValidateAudience = true,
+                    //ValidAudience = settings.Audience,
+                    //ValidateIssuerSigningKey = true,
+                    //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.SecurityKey)),
+                    //RequireExpirationTime = true,
+                    //ValidateLifetime = settings.ExpirationTime.GetValueOrDefault() > TimeSpan.Zero,
+                    //ClockSkew = settings.ClockSkew
                     ValidateIssuer = true,
-                    ValidIssuer = configuration.JWTOptions.Issuer,
+                    ValidIssuer = settings.Issuer,
                     ValidateAudience = true,
-                    ValidAudience = configuration.JWTOptions.Audience,
+                    ValidAudience = settings.Audience,
                     ValidateLifetime = true,
                     ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.JWTOptions.SecurityKey)),
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.SecurityKey)),
                     RequireExpirationTime = true,
-                    ClockSkew = TimeSpan.Zero
+                    ClockSkew = settings.ClockSkew
                 };
             });
 
         services.Configure<IdentityOptions>(options =>
         {
-            options.User.RequireUniqueEmail = configuration.JWTOptions.RequireUniqueEmail;
+            options.User.RequireUniqueEmail = settings.RequireUniqueEmail;
             options.Password = new PasswordOptions
             {
-                RequireDigit = configuration.JWTOptions.RequireDigit,
-                RequiredLength = configuration.JWTOptions.RequiredLength,
-                RequireUppercase = configuration.JWTOptions.RequireUppercase,
-                RequireLowercase = configuration.JWTOptions.RequireLowercase,
-                RequireNonAlphanumeric = configuration.JWTOptions.RequireNonAlphanumeric,
-                RequiredUniqueChars = configuration.JWTOptions.RequiredUniqueChars
+                RequireDigit = settings.RequireDigit,
+                RequiredLength = settings.RequiredLength,
+                RequireUppercase = settings.RequireUppercase,
+                RequireLowercase = settings.RequireLowercase,
+                RequireNonAlphanumeric = settings.RequireNonAlphanumeric,
+                RequiredUniqueChars = settings.RequiredUniqueChars
             };
 
-            options.SignIn.RequireConfirmedEmail = configuration.JWTOptions.RequireConfirmedEmail;
+            options.SignIn.RequireConfirmedEmail = settings.RequireConfirmedEmail;
             options.Lockout = new LockoutOptions
             {
-                MaxFailedAccessAttempts = configuration.JWTOptions.MaxFailedAccessAttempts,
-                AllowedForNewUsers = configuration.JWTOptions.AllowedForNewUsers,
-                DefaultLockoutTimeSpan = configuration.JWTOptions.DefaultLockoutTimeSpan
+                MaxFailedAccessAttempts = settings.MaxFailedAccessAttempts,
+                AllowedForNewUsers = settings.AllowedForNewUsers,
+                DefaultLockoutTimeSpan = settings.DefaultLockoutTimeSpan
             };
         });
-
-        return services;
-    }
-
-    internal static IServiceCollection AddOptionsConfiguration(this IServiceCollection services, IConfiguration configuration)
-    {
-        services
-            .Configure<JsonOptions>(options =>
-            {
-                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
-                options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-                options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
-                options.JsonSerializerOptions.WriteIndented = true;
-            })
-            .Configure<JwtOptions>(configuration.GetSection("JwtOptions"))
-            .Configure<ValidationOptions>(configuration.GetSection("ValidationOptions"))
-            .Configure<RouteOptions>(options => options.LowercaseUrls = true)
-            .Configure<KestrelServerOptions>(configuration.GetSection("Kestrel"));
 
         return services;
     }
