fcafbd07623d8a1e5b277f928885f9eb1fb4d53c
Angelo Pirola <angelo9871@gmail.com>
2025-08-12 10:37:02 +0200

    Add exception messages to MessagesExceptions class
    Added several constant string messages to the `MessagesExceptions` class in the `MinimalApi.Identity.Core.Utility.Messages` namespace. These messages cover user, general, database, and authentication exceptions, enhancing error handling clarity throughout the application.

--- src/MinimalApi.Identity.Core/Utility/Messages/MessagesExceptions.cs
+++ src/MinimalApi.Identity.Core/Utility/Messages/MessagesExceptions.cs
@@ -2,11 +2,18 @@
 
 public static class MessagesExceptions
 {
+    // User exceptions
     public const string UserNotAuthenticated = "User is not authenticated";
     public const string UserNotHavePermission = "User does not have permission";
+
+    // General exceptions
     public const string ProblemDetailsMessageTitle = "An error occurred while processing your request";
     public const string UnexpectedError = "An unexpected error occurred!";
+
+    // Database exceptions
     public const string ConstraintViolation = "A violation occurred for a database constraint";
+
+    // Authentication exceptions
     public const string InvalidAccessToken = "Invalid access token";
     public const string InvalidRefreshToken = "Invalid refresh token";
 }
