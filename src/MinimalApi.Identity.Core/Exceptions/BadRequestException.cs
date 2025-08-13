d7fae44642f7973f8c7124d025f5c5761643bbee
Angelo Pirola <angelo9871@gmail.com>
2025-08-14 00:01:02 +0200

    Add constructor to BadRequestException for IdentityError
    Updated `BadRequestException` to include a new constructor that accepts an `IEnumerable<IdentityError>` and formats error messages using `GenericExtensions.FormatErrorMessage(errors)`. Removed the private static method `FormatErrorMessage` and its implementation.

--- src/MinimalApi.Identity.Core/Exceptions/BadRequestException.cs
+++ src/MinimalApi.Identity.Core/Exceptions/BadRequestException.cs
@@ -16,16 +16,4 @@ public class BadRequestException : Exception
 
     public BadRequestException(IEnumerable<IdentityError> errors) : base(GenericExtensions.FormatErrorMessage(errors))
     { }
-
-    //private static string FormatErrorMessage(IEnumerable<IdentityError> errors)
-    //{
-    //    var sb = new StringBuilder();
-
-    //    foreach (var error in errors)
-    //    {
-    //        sb.AppendLine($"{error.Code}: {error.Description}");
-    //    }
-
-    //    return sb.ToString();
-    //}
 }
