bcbc986edea3f99d521c0561ed48bef06e8966d3
Angelo Pirola <angelo9871@gmail.com>
2025-08-14 00:01:51 +0200

    Refactor EmailSending class properties
    Removed several properties and added new ones to better align with updated requirements. Commented out unused properties for future reference. This restructuring enhances the class design and functionality.

--- src/MinimalApi.Identity.Core/Entities/EmailSending.cs
+++ src/MinimalApi.Identity.Core/Entities/EmailSending.cs
@@ -1,16 +1,25 @@
 ﻿using MinimalApi.Identity.Core.Entities.Common;
-using MinimalApi.Identity.Core.Enums;
 
 namespace MinimalApi.Identity.Core.Entities;
 
 public class EmailSending : BaseEntity, IEntity
 {
-    public EmailSendingType EmailSendingType { get; set; }
     public string EmailTo { get; set; } = null!;
     public string Subject { get; set; } = null!;
     public string Body { get; set; } = null!;
-    public bool Sent { get; set; }
-    public DateTime DateSent { get; set; }
-    public string? ErrorMessage { get; set; } //Consente valore NULL
-    public string? ErrorDetails { get; set; } //Consente valore NULL
+    //public int EmailSendingStatusId { get; set; }
+    //public EmailSendingStatus EmailSendingStatus { get; set; } = null!;
+    //public int EmailSendingId { get; set; }
+    //public EmailSending EmailSending { get; set; } = null!;
+    public int TypeEmailSendingId { get; set; }
+    public TypeEmailSending TypeEmailSending { get; set; } = null!;
+    //public bool Sent { get; set; }
+    public int TypeEmailStatusId { get; set; }
+    public TypeEmailStatus TypeEmailStatus { get; set; } = null!;
+    public DateTime DateSent { get; set; } // Data e ora in cui l'email è stata inviata con successo
+    public int RetrySender { get; set; } // Numero di tentativi di invio dell'email - Numero tentativi: 10 (default)
+    //public TimeSpan DelayOnError { get; set; } // Tempo di attesa tra i tentativi di invio dell'email in caso di errore - Default: 00:00:30 (30 secondi)
+    public DateTime? RetrySenderDate { get; set; } // Data e ora dell'ultimo tentativo di invio dell'email
+    public string? RetrySenderErrorMessage { get; set; } //Consente valore NULL
+    public string? RetrySenderErrorDetails { get; set; } //Consente valore NULL
 }
