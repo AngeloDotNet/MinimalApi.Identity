77706cf03f0d76df9eca26355d0c87d9e96abbf9
Angelo Pirola <angelo9871@gmail.com>
2025-08-20 08:49:14 +0200

    Refactor IEmailSavingService interface
    Removed unnecessary using directive, namespace declaration, and the SaveEmailAsync method signature to simplify the interface and reduce dependencies.

--- src/MinimalApi.Identity.API/Services/Interfaces/IEmailSavingService.cs
+++
@@ -1,8 +0,0 @@
-﻿using MinimalApi.Identity.Core.Entities;
-
-namespace MinimalApi.Identity.API.Services.Interfaces;
-
-public interface IEmailSavingService
-{
-    Task SaveEmailAsync(EmailSending email);
-}
