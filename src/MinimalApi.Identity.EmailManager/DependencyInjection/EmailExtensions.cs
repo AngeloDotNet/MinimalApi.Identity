204a4a2a25b76fc3d159c331c3ff66d7e31d704c
Angelo Pirola <angelo9871@gmail.com>
2025-08-23 00:05:20 +0200

    Add EmailExtensions for email management service registration
    Introduces a new static class `EmailExtensions` in the
    `MinimalApi.Identity.EmailManager.DependencyInjection` namespace.
    The `EmailManagerRegistrationService` method extends
    `IServiceCollection` to register email management services,
    including an email manager service interface and a mail sender
    implementation, with appropriate lifetimes for each service.

---
+++ src/MinimalApi.Identity.EmailManager/DependencyInjection/EmailExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MinimalApi.Identity.Core.DependencyInjection;
+using MinimalApi.Identity.EmailManager.HostedServices;
+using MinimalApi.Identity.EmailManager.SenderEmail;
+using MinimalApi.Identity.EmailManager.Services;
+
+namespace MinimalApi.Identity.EmailManager.DependencyInjection;
+
+public static class EmailExtensions
+{
+    public static IServiceCollection EmailManagerRegistrationService(this IServiceCollection services)
+    {
+        services
+            .AddRegisterServices(options =>
+            {
+                options.Interfaces = [typeof(IEmailManagerService)];
+                options.StringEndsWith = "Service";
+                options.Lifetime = ServiceLifetime.Transient;
+            })
+            .AddSingleton<IMailKitEmailSender, MailKitEmailSender>()
+            .AddSingleton<IHostedService, BackgroundEmailSender>();
+
+        return services;
+    }
+}
