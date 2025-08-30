0bff37dffb33239332edb88bd43543c9a3dbc61e
Angelo Pirola <angelo9871@gmail.com>
2025-08-31 00:07:32 +0200

    Add ProfileMapper class for UserProfile conversion
    Introduces a new static class `ProfileMapper` in the
    `MinimalApi.Identity.API.Mapping` namespace with a method
    `ToEntity` that converts `UserProfile` objects to
    `UserProfileModel`. Removes the deprecated commented-out
    `ToEntityModel` method for converting `PolicyDetailsResponseModel`
    to `AuthPolicy`.

---
+++ src/MinimalApi.Identity.ProfileManager/Mapping/ProfileMapper.cs
@@ -0,0 +1,31 @@
+﻿using MinimalApi.Identity.Core.Entities;
+using MinimalApi.Identity.ProfileManager.Models;
+
+namespace MinimalApi.Identity.API.Mapping;
+
+internal static class ProfileMapper
+{
+    internal static UserProfileModel ToEntity(this UserProfile userProfile)
+    {
+        return new UserProfileModel(
+            userProfile.UserId,
+            userProfile.User.Email!,
+            userProfile.FirstName,
+            userProfile.LastName,
+            userProfile.IsEnabled,
+            userProfile.LastDateChangePassword
+        );
+    }
+
+    //public static AuthPolicy ToEntityModel(this PolicyDetailsResponseModel model)
+    //{
+    //    return new AuthPolicy
+    //    {
+    //        PolicyName = model.PolicyName,
+    //        PolicyDescription = model.PolicyDescription,
+    //        PolicyPermissions = model.PolicyPermissions,
+    //        IsDefault = model.IsDefault,
+    //        IsActive = model.IsActive
+    //    };
+    //}
+}
