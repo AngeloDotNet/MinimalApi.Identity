aa878440a8fb02065fc75680bd7344583d412cc7
Angelo Pirola <angelo9871@gmail.com>
2025-09-01 00:07:13 +0200

    Disable AuthorizationPolicyGeneration functionality
    Commented out the entire `AuthorizationPolicyGeneration` class, including the `StartAsync` and `StopAsync` methods. Removed using directives for `Microsoft.Extensions.DependencyInjection`, `Microsoft.Extensions.Hosting`, and `Microsoft.Extensions.Logging`, indicating a potential refactor or change in dependencies.

--- src/MinimalApi.Identity.PolicyManager/HostedServices/AuthorizationPolicyGeneration.cs
+++ src/MinimalApi.Identity.PolicyManager/HostedServices/AuthorizationPolicyGeneration.cs
@@ -1,31 +1,26 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using MinimalApi.Identity.PolicyManager.Services;
+﻿namespace MinimalApi.Identity.PolicyManager.HostedServices;
 
-namespace MinimalApi.Identity.PolicyManager.HostedServices;
+//public class AuthorizationPolicyGeneration(IServiceProvider serviceProvider, ILogger<AuthorizationPolicyGeneration> logger) : IHostedService
+//{
+//    public async Task StartAsync(CancellationToken cancellationToken)
+//    {
+//        using var scope = serviceProvider.CreateAsyncScope();
 
-public class AuthorizationPolicyGeneration(IServiceProvider serviceProvider, ILogger<AuthorizationPolicyGeneration> logger) : IHostedService
-{
-    public async Task StartAsync(CancellationToken cancellationToken)
-    {
-        using var scope = serviceProvider.CreateAsyncScope();
+//        var authPolicyService = scope.ServiceProvider.GetRequiredService<IAuthPolicyService>();
+//        var result = await authPolicyService.GenerateAuthPoliciesAsync();
 
-        var authPolicyService = scope.ServiceProvider.GetRequiredService<IAuthPolicyService>();
-        var result = await authPolicyService.GenerateAuthPoliciesAsync();
+//        if (result)
+//        {
+//            logger.LogInformation("Authorization policies generated successfully.");
+//        }
+//        else
+//        {
+//            logger.LogWarning("An error occurred while generating authorization policies.");
+//        }
+//    }
 
-        if (result)
-        {
-            logger.LogInformation("Authorization policies generated successfully.");
-        }
-        else
-        {
-            logger.LogWarning("An error occurred while generating authorization policies.");
-        }
-    }
-
-    public Task StopAsync(CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
-}
+//    public Task StopAsync(CancellationToken cancellationToken)
+//    {
+//        return Task.CompletedTask;
+//    }
+//}
