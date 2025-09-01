5dda91277fe32d4c8ebd28c7d44bc5af935a9af9
Angelo Pirola <angelo9871@gmail.com>
2025-09-02 00:03:02 +0200

    Refactor AuthPolicyService: Remove policy methods
    Removed `GenerateAuthPoliciesAsync` and `UpdateAuthPoliciesAsync` methods, commenting out their logic for potential future use. Updated using directives to streamline dependencies, adding `MinimalApi.Identity.Core.Database` while removing several others.

--- src/MinimalApi.Identity.PolicyManager/Services/AuthPolicyService.cs
+++ src/MinimalApi.Identity.PolicyManager/Services/AuthPolicyService.cs
@@ -1,11 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using MinimalApi.Identity.Core.Authorization;
-using MinimalApi.Identity.Core.Configurations;
+﻿using Microsoft.Extensions.Logging;
 using MinimalApi.Identity.Core.Database;
-using MinimalApi.Identity.Core.Exceptions;
 using MinimalApi.Identity.PolicyManager.DependencyInjection;
 using MinimalApi.Identity.PolicyManager.Models;
 using MinimalApi.Identity.PolicyManager.Services;
@@ -23,90 +17,90 @@ public class AuthPolicyService(MinimalApiAuthDbContext dbContext, ILogger<AuthPo
     public async Task<string> DeletePolicyAsync(DeletePolicyModel model, CancellationToken cancellationToken)
         => await PolicyQuery.DeletePolicyAsync(model, dbContext, cancellationToken);
 
-    public async Task<bool> GenerateAuthPoliciesAsync()
-    {
-        try
-        {
-            using var scope = serviceProvider.CreateScope();
+    //public async Task<bool> GenerateAuthPoliciesAsync()
+    //{
+    //    try
+    //    {
+    //        using var scope = serviceProvider.CreateScope();
 
-            var listPolicy = await PolicyQuery.GetPoliciesAsync(dbContext, p => p.IsActive);
-            var authorizationOptions = serviceProvider.GetRequiredService<IOptions<AuthorizationOptions>>().Value;
+    //        var listPolicy = await PolicyQuery.GetPoliciesAsync(dbContext, p => p.IsActive);
+    //        var authorizationOptions = serviceProvider.GetRequiredService<IOptions<AuthorizationOptions>>().Value;
 
-            if (listPolicy.Count == 0)
-            {
-                logger.LogWarning(ConstantsConfiguration.NoActivePoliciesFound);
-                throw new NotFoundException();
-            }
+    //        if (listPolicy.Count == 0)
+    //        {
+    //            logger.LogWarning(ConstantsConfiguration.NoActivePoliciesFound);
+    //            throw new NotFoundException();
+    //        }
 
-            logger.LogInformation("Found {PolicyCount} active policies in the database.", listPolicy.Count);
+    //        logger.LogInformation("Found {PolicyCount} active policies in the database.", listPolicy.Count);
 
-            foreach (var policy in listPolicy)
-            {
-                if (authorizationOptions.GetPolicy(policy.PolicyName) != null)
-                {
-                    logger.LogInformation("Policy {PolicyName} already exists.", policy.PolicyName);
-                    continue;
-                }
+    //        foreach (var policy in listPolicy)
+    //        {
+    //            if (authorizationOptions.GetPolicy(policy.PolicyName) != null)
+    //            {
+    //                logger.LogInformation("Policy {PolicyName} already exists.", policy.PolicyName);
+    //                continue;
+    //            }
 
-                var permissionRequirement = new PermissionRequirement(policy.PolicyPermissions);
-                logger.LogInformation("Adding policy: {PolicyName} with permissions: {PolicyPermissions}", policy.PolicyName, string.Join(", ", policy.PolicyPermissions));
-                authorizationOptions.AddPolicy(policy.PolicyName, policy => policy.Requirements.Add(permissionRequirement));
-            }
+    //            var permissionRequirement = new PermissionRequirement(policy.PolicyPermissions);
+    //            logger.LogInformation("Adding policy: {PolicyName} with permissions: {PolicyPermissions}", policy.PolicyName, string.Join(", ", policy.PolicyPermissions));
+    //            authorizationOptions.AddPolicy(policy.PolicyName, policy => policy.Requirements.Add(permissionRequirement));
+    //        }
 
-            return true;
-        }
-        catch (NotFoundException ex)
-        {
-            logger.LogWarning(ex, ConstantsConfiguration.NoActivePoliciesFound);
-            return false;
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "An error occurred while generating authorization policies.");
-            return false;
-        }
-    }
+    //        return true;
+    //    }
+    //    catch (NotFoundException ex)
+    //    {
+    //        logger.LogWarning(ex, ConstantsConfiguration.NoActivePoliciesFound);
+    //        return false;
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        logger.LogError(ex, "An error occurred while generating authorization policies.");
+    //        return false;
+    //    }
+    //}
 
-    public async Task<bool> UpdateAuthPoliciesAsync()
-    {
-        try
-        {
-            using var scope = serviceProvider.CreateScope();
+    //public async Task<bool> UpdateAuthPoliciesAsync()
+    //{
+    //    try
+    //    {
+    //        using var scope = serviceProvider.CreateScope();
 
-            var listPolicy = await PolicyQuery.GetPoliciesAsync(dbContext, p => p.IsActive);
-            var authorizationOptions = serviceProvider.GetRequiredService<IOptions<AuthorizationOptions>>().Value;
+    //        var listPolicy = await PolicyQuery.GetPoliciesAsync(dbContext, p => p.IsActive);
+    //        var authorizationOptions = serviceProvider.GetRequiredService<IOptions<AuthorizationOptions>>().Value;
 
-            if (listPolicy.Count == 0)
-            {
-                logger.LogWarning(ConstantsConfiguration.NoActivePoliciesFound);
-                throw new NotFoundException();
-            }
+    //        if (listPolicy.Count == 0)
+    //        {
+    //            logger.LogWarning(ConstantsConfiguration.NoActivePoliciesFound);
+    //            throw new NotFoundException();
+    //        }
 
-            logger.LogInformation("Found {PolicyCount} active policies in the database.", listPolicy.Count);
-            foreach (var policy in listPolicy)
-            {
-                if (authorizationOptions.GetPolicy(policy.PolicyName) != null)
-                {
-                    logger.LogInformation("Policy {PolicyName} already exists.", policy.PolicyName);
-                    continue;
-                }
+    //        logger.LogInformation("Found {PolicyCount} active policies in the database.", listPolicy.Count);
+    //        foreach (var policy in listPolicy)
+    //        {
+    //            if (authorizationOptions.GetPolicy(policy.PolicyName) != null)
+    //            {
+    //                logger.LogInformation("Policy {PolicyName} already exists.", policy.PolicyName);
+    //                continue;
+    //            }
 
-                var permissionRequirement = new PermissionRequirement(policy.PolicyPermissions);
-                logger.LogInformation("Adding policy: {PolicyName} with permissions: {PolicyPermissions}", policy.PolicyName, string.Join(", ", policy.PolicyPermissions));
-                authorizationOptions.AddPolicy(policy.PolicyName, policy => policy.Requirements.Add(permissionRequirement));
-            }
+    //            var permissionRequirement = new PermissionRequirement(policy.PolicyPermissions);
+    //            logger.LogInformation("Adding policy: {PolicyName} with permissions: {PolicyPermissions}", policy.PolicyName, string.Join(", ", policy.PolicyPermissions));
+    //            authorizationOptions.AddPolicy(policy.PolicyName, policy => policy.Requirements.Add(permissionRequirement));
+    //        }
 
-            return true;
-        }
-        catch (NotFoundException ex)
-        {
-            logger.LogWarning(ex, ConstantsConfiguration.NoActivePoliciesFound);
-            return false;
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "An error occurred while generating authorization policies.");
-            return false;
-        }
-    }
+    //        return true;
+    //    }
+    //    catch (NotFoundException ex)
+    //    {
+    //        logger.LogWarning(ex, ConstantsConfiguration.NoActivePoliciesFound);
+    //        return false;
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        logger.LogError(ex, "An error occurred while generating authorization policies.");
+    //        return false;
+    //    }
+    //}
 }
