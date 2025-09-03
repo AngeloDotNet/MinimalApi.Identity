32fd763a3da2f76f9578b9569c3e0808238e3b26
Angelo Pirola <angelo9871@gmail.com>
2025-09-03 11:04:47 +0200

    Add ProgramOptions class for application settings
    Introduces the `ProgramOptions` class in the `MinimalApi.Identity.API.Options` namespace. This class includes properties for `JwtOptions`, `FeatureFlagsOptions`, `DatabaseType`, `MigrationsAssembly`, and `FormatErrors`, each initialized with default values to streamline the management of application configuration settings.

---
+++ src/MinimalApi.Identity.API/Options/ProgramOptions.cs
@@ -0,0 +1,12 @@
+﻿using MinimalApi.Identity.Core.Enums;
+
+namespace MinimalApi.Identity.API.Options;
+
+public class ProgramOptions
+{
+    public JwtOptions JwtOptions { get; set; } = new JwtOptions();
+    public FeatureFlagsOptions FeatureFlagsOptions { get; set; } = new FeatureFlagsOptions();
+    public string DatabaseType { get; set; } = "sqlserver";
+    public string MigrationsAssembly { get; set; } = string.Empty;
+    public ErrorResponseFormat FormatErrors { get; set; } = ErrorResponseFormat.Default;
+}
