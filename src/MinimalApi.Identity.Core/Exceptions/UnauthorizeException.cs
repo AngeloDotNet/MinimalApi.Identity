8c17835889acacf4b998c86daf447eea94182104
Angelo Pirola <angelo9871@gmail.com>
2025-09-03 11:03:04 +0200

    Add UnauthorizeException class for specific error handling
    Introduces a new `UnauthorizeException` class in the
    `MinimalApi.Identity.Core.Exceptions` namespace. This
    class extends the base `Exception` class and provides
    three constructors for enhanced exception handling
    related to unauthorized access scenarios.

---
+++ src/MinimalApi.Identity.Core/Exceptions/UnauthorizeException.cs
@@ -0,0 +1,13 @@
+﻿namespace MinimalApi.Identity.Core.Exceptions;
+
+public class UnauthorizeException : Exception
+{
+    public UnauthorizeException()
+    { }
+
+    public UnauthorizeException(string? message) : base(message)
+    { }
+
+    public UnauthorizeException(string? message, Exception? innerException) : base(message, innerException)
+    { }
+}
