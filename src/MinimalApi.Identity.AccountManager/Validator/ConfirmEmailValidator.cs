ffbe9444c67fd2e12ff6f5c16b07e4899752358f
Angelo Pirola <angelo9871@gmail.com>
2025-09-06 00:03:22 +0200

    Add ConfirmEmailValidator for user email confirmation
    Introduces the `ConfirmEmailValidator` class in the
    `MinimalApi.Identity.AccountManager.Validator` namespace.
    This validator checks that the `UserId` and `Token`
    properties of `ConfirmEmailModel` are not empty,
    providing appropriate error messages. Necessary
    using directives for `FluentValidation` and the model
    are also included.

---
+++ src/MinimalApi.Identity.AccountManager/Validator/ConfirmEmailValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MinimalApi.Identity.AccountManager.Models;
+
+namespace MinimalApi.Identity.AccountManager.Validator;
+
+public class ConfirmEmailValidator : AbstractValidator<ConfirmEmailModel>
+{
+    public ConfirmEmailValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("User ID is required.");
+
+        RuleFor(x => x.Token)
+            .NotEmpty().WithMessage("Token is required.");
+    }
+}
