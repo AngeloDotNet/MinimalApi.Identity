4dea10166c949d20e8cf450ccaabacf760a95776
Angelo Pirola <angelo9871@gmail.com>
2025-09-06 00:02:36 +0200

    Add AccountExtensions for account management services
    Introduces a new static class `AccountExtensions` in the
    `MinimalApi.Identity.AccountManager.DependencyInjection` namespace.
    This class defines the `/change-email` endpoint and includes
    the `AccountManagerRegistrationService` method to register
    account management services with `IServiceCollection`.
    FluentValidation is configured for `ChangeEmailValidator`,
    and several commented-out email confirmation endpoints were removed.

---
+++ src/MinimalApi.Identity.AccountManager/DependencyInjection/AccountExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MinimalApi.Identity.AccountManager.Services;
+using MinimalApi.Identity.AccountManager.Validator;
+using MinimalApi.Identity.Core.DependencyInjection;
+
+namespace MinimalApi.Identity.AccountManager.DependencyInjection;
+
+public static class AccountExtensions
+{
+    public const string EndpointChangeEmail = "/change-email";
+    //public const string EndpointsConfirmEmail = "/confirm-email/{userId}/{token}";
+    //public const string EndpointsConfirmEmailChange = "/confirm-email-change/{userId}/{email}/{token}";
+
+    public static IServiceCollection AccountManagerRegistrationService(this IServiceCollection services)
+    {
+        services
+            .AddRegisterServices(options =>
+            {
+                options.Interfaces = [typeof(IAccountService)];
+                options.StringEndsWith = "Service";
+                options.Lifetime = ServiceLifetime.Transient;
+            })
+            .ConfigureFluentValidation<ChangeEmailValidator>();
+
+        return services;
+    }
+}
