9ec1a0f06780c70fe39987c0cf93800f6484d150
Angelo Pirola <angelo9871@gmail.com>
2025-09-08 00:01:04 +0200

    Add AuthExtensions for service registration
    Introduces a new static class `AuthExtensions` in the
    `MinimalApi.Identity.AuthManager.DependencyInjection` namespace.
    Adds the `AuthManagerRegistrationService` method to register
    the `AuthenticationStartupTask` hosted service. Commented-out
    lines indicate previous service registration intentions.

---
+++ src/MinimalApi.Identity.AuthManager/DependencyInjection/AuthExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MinimalApi.Identity.AuthManager.HostedServices;
+
+namespace MinimalApi.Identity.AuthManager.DependencyInjection;
+
+public static class AuthExtensions
+{
+    public static IServiceCollection AuthManagerRegistrationService(this IServiceCollection services)
+    {
+        services
+            //.AddRegisterServices(options =>
+            //{
+            //    options.Interfaces = [typeof(IEmailManagerService)];
+            //    options.StringEndsWith = "Service";
+            //    options.Lifetime = ServiceLifetime.Transient;
+            //})
+            //.AddSingleton<IMailKitEmailSender, MailKitEmailSender>()
+            .AddHostedService<AuthenticationStartupTask>();
+
+        return services;
+    }
+}
