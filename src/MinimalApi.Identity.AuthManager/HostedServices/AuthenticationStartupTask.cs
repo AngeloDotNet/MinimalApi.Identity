2da46ff54cab81bb6f1fe1b0cfaa4f51e9ed9c46
Angelo Pirola <angelo9871@gmail.com>
2025-09-09 00:15:18 +0200

    Remove role creation logic and add admin user setup
    This commit removes the commented-out code for checking and creating roles (Administrator, PowerUser, User). It also introduces the creation of an `ApplicationUser` instance, setting the username and email from `usersOptions` for the administrator's credentials.

--- src/MinimalApi.Identity.AuthManager/HostedServices/AuthenticationStartupTask.cs
+++ src/MinimalApi.Identity.AuthManager/HostedServices/AuthenticationStartupTask.cs
@@ -19,18 +19,8 @@ public class AuthenticationStartupTask(IServiceProvider serviceProvider, IConfig
         configuration.Bind(nameof(UsersOptions), usersOptions);
 
         var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
-        //var roleNames = new string[] { RoleNames.Administrator, RoleNames.PowerUser, RoleNames.User };
-
-        //foreach (var roleName in roleNames)
-        //{
-        //    var roleExists = await roleManager.RoleExistsAsync(roleName);
-        //    if (!roleExists)
-        //    {
-        //        await roleManager.CreateAsync(new ApplicationRole(roleName));
-        //    }
-        //}
-
         var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
         var administratorUser = new ApplicationUser
         {
             UserName = usersOptions.AssignAdminUsername,
