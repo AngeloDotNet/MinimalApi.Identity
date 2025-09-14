1abe350f3f7453a1819e2676c0aa495886434c5a
Angelo Pirola <angelo9871@gmail.com>
2025-09-14 17:01:37 +0200

    Add UtcDateTimeConverter for JSON DateTime handling
    Introduce a `UtcDateTimeConverter` class in the `MinimalApi.Identity.Core.Converter` namespace to handle serialization and deserialization of `DateTime` values in UTC format.
    
    The class inherits from `JsonConverter<DateTime>` and overrides the `Read` and `Write` methods to ensure `DateTime` values are converted to and serialized in UTC.
    
    It supports an optional `serializationFormat` parameter for custom date formatting, defaulting to `"yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffffff'Z'"`.
    
    Additional `using` directives were added to include required namespaces.

---
+++ src/MinimalApi.Identity.Core/Converter/UtcDateTimeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MinimalApi.Identity.Core.Converter;
+
+public class UtcDateTimeConverter(string? serializationFormat) : JsonConverter<DateTime>
+{
+    private readonly string serializationFormat = serializationFormat ?? "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffffff'Z'";
+
+    public UtcDateTimeConverter() : this(null)
+    { }
+
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        => reader.GetDateTime().ToUniversalTime();
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        => writer.WriteStringValue((value.Kind == DateTimeKind.Local ? value.ToUniversalTime() : value).ToString(serializationFormat));
+}
