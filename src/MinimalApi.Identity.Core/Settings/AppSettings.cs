1dca07dcd954403eb847aa6f4a68acef10b18285
Angelo Pirola <angelo9871@gmail.com>
2025-09-18 00:06:37 +0200

    Make AppSettings properties mutable with 'set' accessors
    Updated the `AppSettings` class to replace `init` accessors with
    `set` accessors for all properties. This change allows the
    properties to be modified after object initialization, improving
    flexibility in their usage.

--- src/MinimalApi.Identity.Core/Settings/AppSettings.cs
+++ src/MinimalApi.Identity.Core/Settings/AppSettings.cs
@@ -2,18 +2,18 @@
 
 public class AppSettings
 {
-    public string AssignAdminUsername { get; init; } = null!;
-    public string AssignAdminEmail { get; init; } = null!;
-    public string AssignAdminPassword { get; init; } = null!;
-    public int PasswordExpirationDays { get; init; } = 90;
-    public int IntervalEmailSenderMinutes { get; init; } = 5;
-    public string ErrorResponseFormat { get; init; } = "Default";
-    public bool EnabledFeatureLicense { get; init; } = true;
-    public bool EnabledFeatureModule { get; init; } = true;
-    public int ValidateMinLength { get; init; } = 3;
-    public int ValidateMaxLength { get; init; } = 50;
-    public int ValidateMinLengthDescription { get; init; } = 5;
-    public int ValidateMaxLengthDescription { get; init; } = 100;
-    public string DatabaseType { get; init; } = null!;
-    public string MigrationsAssembly { get; init; } = null!;
+    public string AssignAdminUsername { get; set; } = null!;
+    public string AssignAdminEmail { get; set; } = null!;
+    public string AssignAdminPassword { get; set; } = null!;
+    public int PasswordExpirationDays { get; set; } = 90;
+    public int IntervalEmailSenderMinutes { get; set; } = 5;
+    public string ErrorResponseFormat { get; set; } = "Default";
+    public bool EnabledFeatureLicense { get; set; } = true;
+    public bool EnabledFeatureModule { get; set; } = true;
+    public int ValidateMinLength { get; set; } = 3;
+    public int ValidateMaxLength { get; set; } = 50;
+    public int ValidateMinLengthDescription { get; set; } = 5;
+    public int ValidateMaxLengthDescription { get; set; } = 100;
+    public string DatabaseType { get; set; } = null!;
+    public string MigrationsAssembly { get; set; } = null!;
 }
