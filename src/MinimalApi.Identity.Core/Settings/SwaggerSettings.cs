58cb44941897ec5b147d9cf21c002c115d08850b
Angelo Pirola <angelo9871@gmail.com>
2025-09-22 00:39:41 +0200

    Add SwaggerSettings class for Swagger configuration
    Introduced a new `SwaggerSettings` class in the `MinimalApi.Identity.Core.Settings` namespace. This class includes properties for enabling/disabling Swagger (`IsEnabled`) and optional authentication credentials (`UserName` and `Password`). This change provides a centralized way to configure Swagger-related settings.

---
+++ src/MinimalApi.Identity.Core/Settings/SwaggerSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace MinimalApi.Identity.Core.Settings;
+
+public class SwaggerSettings
+{
+    public bool IsEnabled { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+}
