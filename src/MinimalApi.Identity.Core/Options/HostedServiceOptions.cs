80a6d8ede7312341a3ae82b0414aa3e4740cc2dc
Angelo Pirola <angelo9871@gmail.com>
2025-09-22 00:41:21 +0200

    Remove HostedServiceOptions class and related imports
    The `HostedServiceOptions` class was removed, including its
    `IntervalEmailSenderMinutes` property, which was previously
    annotated with the `[Required]` attribute. Additionally, the
    `using System.ComponentModel.DataAnnotations;` directive was
    removed as it is no longer needed. This change suggests the
    class is no longer required or has been replaced by another
    implementation.

--- src/MinimalApi.Identity.Core/Options/HostedServiceOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace MinimalApi.Identity.API.Options;
-
-public class HostedServiceOptions
-{
-    [Required]
-    public int IntervalEmailSenderMinutes { get; init; }
-}
