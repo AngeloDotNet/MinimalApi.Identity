81d033909727e00735709b3269a4f1f84c3371b4
Angelo Pirola <angelo9871@gmail.com>
2025-09-21 09:30:20 +0200

    Add string extension methods for value checks
    Introduced three overloaded `HasValue` extension methods for
    the `string?` type to simplify null, empty, and whitespace
    checks. These methods leverage the `[NotNullWhen(true)]`
    attribute to improve nullability annotations. Added the
    `System.Diagnostics.CodeAnalysis` namespace to support this
    attribute.

--- src/MinimalApi.Identity.Core/Extensions/GenericExtensions.cs
+++ src/MinimalApi.Identity.Core/Extensions/GenericExtensions.cs
@@ -1,10 +1,20 @@
-﻿using System.Text;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
 using Microsoft.AspNetCore.Identity;
 
 namespace MinimalApi.Identity.Core.Extensions;
 
 public static class GenericExtensions
 {
+    public static bool HasValue([NotNullWhen(true)] this string? input)
+        => input.HasValue(allowEmptyString: false, whiteSpaceAsEmpty: true);
+
+    public static bool HasValue([NotNullWhen(true)] this string? input, bool allowEmptyString)
+        => input.HasValue(allowEmptyString, whiteSpaceAsEmpty: true);
+
+    public static bool HasValue([NotNullWhen(true)] this string? input, bool allowEmptyString, bool whiteSpaceAsEmpty)
+        => allowEmptyString ? input is not null : whiteSpaceAsEmpty ? !string.IsNullOrWhiteSpace(input) : !string.IsNullOrEmpty(input);
+
     internal static string FormatErrorMessage(IEnumerable<IdentityError> errors)
     {
         var sb = new StringBuilder();
