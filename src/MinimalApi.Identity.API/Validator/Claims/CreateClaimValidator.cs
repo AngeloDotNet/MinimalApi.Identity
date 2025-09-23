c241bd1448c8c9e41280e67cc36d4e3012830644
Angelo Pirola <angelo9871@gmail.com>
2025-09-24 00:25:45 +0200

    Refactor CreateClaimValidator to use AppSettings
    Replaced `ValidationOptions` with `AppSettings` in the `CreateClaimValidator` class, updating the `using` directive and constructor dependency. Renamed `validationOptions` to `vOptions` for consistency. Updated validation rules to reference `ValidateMinLength` and `ValidateMaxLength` from the new `AppSettings` structure.

--- src/MinimalApi.Identity.API/Validator/Claims/CreateClaimValidator.cs
+++ src/MinimalApi.Identity.API/Validator/Claims/CreateClaimValidator.cs
@@ -1,22 +1,22 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.Options;
 using MinimalApi.Identity.API.Models;
-using MinimalApi.Identity.Core.Options;
+using MinimalApi.Identity.Core.Settings;
 
 namespace MinimalApi.Identity.API.Validator.Claims;
 
 public class CreateClaimValidator : AbstractValidator<CreateClaimModel>
 {
-    public CreateClaimValidator(IOptions<ValidationOptions> options)
+    public CreateClaimValidator(IOptions<AppSettings> options)
     {
-        var validationOptions = options.Value;
+        var vOptions = options.Value;
 
         RuleFor(x => x.Type)
             .NotEmpty().WithMessage("Type is required");
 
         RuleFor(x => x.Value)
             .NotEmpty().WithMessage("Value is required")
-            .MinimumLength(validationOptions.MinLength).WithMessage($"Claim must be at least {validationOptions.MinLength} characters")
-            .MaximumLength(validationOptions.MaxLength).WithMessage($"Claim must not exceed {validationOptions.MaxLength} characters");
+            .MinimumLength(vOptions.ValidateMinLength).WithMessage($"Claim must be at least {vOptions.ValidateMinLength} characters")
+            .MaximumLength(vOptions.ValidateMaxLength).WithMessage($"Claim must not exceed {vOptions.ValidateMaxLength} characters");
     }
 }
