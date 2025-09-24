df109ada0f29093d5c14bf6bff7c480abddadb02
Angelo Pirola <angelo9871@gmail.com>
2025-09-25 00:01:22 +0200

    Add AssignLicenseValidator for license assignment validation
    Introduced the `AssignLicenseValidator` class in the
    `MinimalApi.Identity.LicenseManager.Validator` namespace to
    validate `AssignLicenseModel` using FluentValidation.
    
    Validation rules ensure:
    - `UserId` is required and must be an integer > 0.
    - `LicenseId` is required and must be an integer > 0.
    
    Added necessary `using` directives for FluentValidation and
    the `MinimalApi.Identity.LicenseManager.Models` namespace.

---
+++ src/MinimalApi.Identity.LicenseManager/Validator/AssignLicenseValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MinimalApi.Identity.LicenseManager.Models;
+
+namespace MinimalApi.Identity.LicenseManager.Validator;
+
+public class AssignLicenseValidator : AbstractValidator<AssignLicenseModel>
+{
+    public AssignLicenseValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("UserId is required")
+            .GreaterThan(0).WithMessage("UserId must be an integer greater than zero");
+
+        RuleFor(x => x.LicenseId)
+            .NotEmpty().WithMessage("LicenseId is required")
+            .GreaterThan(0).WithMessage("LicenseId must be an integer greater than zero");
+    }
+}
