9cd7f9acd4309ad031bd25d55c4e6049ff86c8e1
Angelo Pirola <angelo9871@gmail.com>
2025-09-25 00:01:45 +0200

    Add ILicenseService interface for license management
    Introduced a new `ILicenseService` interface in the
    `MinimalApi.Identity.LicenseManager.Services` namespace to
    standardize license management operations. The interface
    includes methods for creating, assigning, revoking, deleting,
    and retrieving licenses, as well as checking license expiration
    and retrieving user license claims. Added necessary `using`
    directives to support the interface functionality.

---
+++ src/MinimalApi.Identity.LicenseManager/Services/ILicenseService.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using MinimalApi.Identity.Core.Entities;
+using MinimalApi.Identity.LicenseManager.Models;
+
+namespace MinimalApi.Identity.LicenseManager.Services;
+
+public interface ILicenseService
+{
+    Task<List<LicenseResponseModel>> GetAllLicensesAsync(CancellationToken cancellationToken);
+    Task<string> CreateLicenseAsync(CreateLicenseModel model, CancellationToken cancellationToken);
+    Task<string> AssignLicenseAsync(AssignLicenseModel model, CancellationToken cancellationToken);
+    Task<string> RevokeLicenseAsync(RevokeLicenseModel model, CancellationToken cancellationToken);
+    Task<string> DeleteLicenseAsync(DeleteLicenseModel model, CancellationToken cancellationToken);
+    Task<Claim> GetClaimLicenseUserAsync(ApplicationUser user, CancellationToken cancellationToken);
+    Task<bool> CheckUserLicenseExpiredAsync(ApplicationUser user, CancellationToken cancellationToken);
+}
