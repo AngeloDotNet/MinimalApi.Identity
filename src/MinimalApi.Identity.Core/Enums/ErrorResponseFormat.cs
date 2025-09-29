6599ad33a2fc4a64ff402c844804fa3ec3cc0bff
Angelo Pirola <angelo9871@gmail.com>
2025-09-29 11:54:44 +0200

    Add ErrorResponseFormat enum as a temporary placeholder
    A new `ErrorResponseFormat` enum was added to the
    `MinimalApi.Identity.Core.Enums` namespace with two values:
    `Default` and `List`. This enum is intended to define error
    response formats. A `TODO` comment was included to indicate
    that this enum should eventually be replaced with an equivalent
    enum from the `MinimalApi.Identity.Shared.Results.AspNetCore.Http`
    namespace. This addition serves as an interim solution.

--- src/MinimalApi.Identity.Core/Enums/ErrorResponseFormat.cs
+++ src/MinimalApi.Identity.Core/Enums/ErrorResponseFormat.cs
@@ -1,5 +1,6 @@
 ﻿namespace MinimalApi.Identity.Core.Enums;
 
+//TODO: Sostituire con enum ErrorResponseFormat in MinimalApi.Identity.Shared.Results.AspNetCore.Http
 public enum ErrorResponseFormat
 {
     Default,
