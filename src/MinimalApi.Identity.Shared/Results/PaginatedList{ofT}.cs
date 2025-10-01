5206c430835d03ed4443765c8283399474aa715b
Angelo Pirola <angelo9871@gmail.com>
2025-10-01 08:59:54 +0200

    Add PaginatedList<T> class for pagination support
    A new `PaginatedList<T>` class was introduced in the
    `MinimalApi.Identity.Shared.Results` namespace to provide
    a reusable structure for handling paginated data.
    
    The class includes properties for items, page index, page
    size, total count, and a flag for the presence of a next
    page. Multiple constructors were added to support various
    initialization scenarios, with method chaining used to
    ensure consistent property initialization.

---
+++ src/MinimalApi.Identity.Shared/Results/PaginatedList{ofT}.cs
@@ -0,0 +1,37 @@
+﻿namespace MinimalApi.Identity.Shared.Results;
+
+public class PaginatedList<T>
+{
+    public IEnumerable<T>? Items { get; set; }
+    public int PageIndex { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public bool HasNextPage { get; set; }
+
+    public PaginatedList()
+    { }
+
+    public PaginatedList(IEnumerable<T>? items, bool hasNextPage = false) : this(items, items?.Count() ?? 0, hasNextPage)
+    { }
+
+    public PaginatedList(IEnumerable<T>? items, int totalCount) : this(items, totalCount, totalCount > items?.Count())
+    { }
+
+    public PaginatedList(IEnumerable<T>? items, int totalCount, bool hasNextPage) : this(items, totalCount, 0, items?.Count() ?? 0, hasNextPage)
+    { }
+
+    public PaginatedList(IEnumerable<T>? items, int totalCount, int pageIndex) : this(items, totalCount, pageIndex, items?.Count() ?? 0)
+    { }
+
+    public PaginatedList(IEnumerable<T>? items, int totalCount, int pageIndex, int pageSize) : this(items, totalCount, pageIndex, pageSize, totalCount > pageIndex * pageSize + (items?.Count() ?? 0))
+    { }
+
+    public PaginatedList(IEnumerable<T>? items, int totalCount, int pageIndex, int pageSize, bool hasNextPage)
+    {
+        Items = items;
+        TotalCount = totalCount;
+        PageIndex = pageIndex;
+        PageSize = pageSize;
+        HasNextPage = hasNextPage;
+    }
+}
