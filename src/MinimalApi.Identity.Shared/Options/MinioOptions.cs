10bf66ecb5a721e0581e1a295af693268c80c2fb
Angelo Pirola <angelo9871@gmail.com>
2025-10-02 00:01:51 +0200

    Add MinioOptions class for MinIO configuration
    Introduce the `MinioOptions` class in the `MinimalApi.Identity.Shared.Options` namespace to encapsulate configuration for interacting with a MinIO server.
    
    The class includes the following properties:
    - `Endpoint`: MinIO server endpoint (e.g., `http://127.0.0.1:9000`).
    - `AccessKey`: Access credentials for the MinIO server.
    - `SecretKey`: Secret credentials for the MinIO server.
    - `BucketName`: Name of the bucket (e.g., `logs`).
    - `LogObjectKey`: Prefix for objects (e.g., `serilog-demo.json`).
    
    This addition provides a structured way to manage MinIO-related settings, likely as part of integrating MinIO functionality into the application.

---
+++ src/MinimalApi.Identity.Shared/Options/MinioOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalApi.Identity.Shared.Options;
+
+public class MinioOptions
+{
+    public string Endpoint { get; set; } = null!; // endpoint del server MinIO: http://127.0.0.1:9000
+    public string AccessKey { get; set; } = null!; // credenziali di accesso
+    public string SecretKey { get; set; } = null!; // credenziali di accesso
+    public string BucketName { get; set; } = null!; // nome del bucket: logs
+    public string LogObjectKey { get; set; } = null!; // prefisso per gli oggetti: serilog-demo.json
+}
