092c1b49b23c1bac72e0450449d9481d104f4941
Angelo Pirola <angelo9871@gmail.com>
2025-10-03 08:52:44 +0200

    Refactor AuthPolicyConfiguration for clarity
    Replaced explicit object initializers with implicit ones (`new()`), simplifying the syntax in the `Configure` method of `AuthPolicyConfiguration`. Assigned the array of policies to a variable (`policies`) before passing it to `builder.HasData`, improving readability. No changes were made to the structure or content of the `AuthPolicy` objects. These updates enhance code clarity and leverage modern C# features.

--- src/MinimalApi.Identity.Core/Database/Configurations/AuthPolicyConfiguration.cs
+++ src/MinimalApi.Identity.Core/Database/Configurations/AuthPolicyConfiguration.cs
@@ -9,113 +9,118 @@ public class AuthPolicyConfiguration : IEntityTypeConfiguration<AuthPolicy>
 {
     public void Configure(EntityTypeBuilder<AuthPolicy> builder)
     {
-        builder.HasData(new AuthPolicy
+        var policies = new AuthPolicy[]
         {
-            Id = 1,
-            PolicyName = nameof(Permissions.AuthPolicyRead),
-            PolicyDescription = "AuthPolicyRead",
-            PolicyPermissions = [nameof(Permissions.AuthPolicy), nameof(Permissions.AuthPolicyRead)],
-            IsDefault = true,
-            IsActive = true
-        },
-        new AuthPolicy
-        {
-            Id = 2,
-            PolicyName = nameof(Permissions.AuthPolicyWrite),
-            PolicyDescription = "AuthPolicyWrite",
-            PolicyPermissions = [nameof(Permissions.AuthPolicy), nameof(Permissions.AuthPolicyWrite)],
-            IsDefault = true,
-            IsActive = true
-        },
-        new AuthPolicy
-        {
-            Id = 3,
-            PolicyName = nameof(Permissions.ClaimRead),
-            PolicyDescription = "ClaimRead",
-            PolicyPermissions = [nameof(Permissions.Claim), nameof(Permissions.ClaimRead)],
-            IsDefault = true,
-            IsActive = true
-        },
-        new AuthPolicy
-        {
-            Id = 4,
-            PolicyName = nameof(Permissions.ClaimWrite),
-            PolicyDescription = "ClaimWrite",
-            PolicyPermissions = [nameof(Permissions.Claim), nameof(Permissions.ClaimWrite)],
-            IsDefault = true,
-            IsActive = true
-        },
-        new AuthPolicy
-        {
-            Id = 5,
-            PolicyName = nameof(Permissions.LicenzaRead),
-            PolicyDescription = "LicenzaRead",
-            PolicyPermissions = [nameof(Permissions.Licenza), nameof(Permissions.LicenzaRead)],
-            IsDefault = true,
-            IsActive = true
-        },
-        new AuthPolicy
-        {
-            Id = 6,
-            PolicyName = nameof(Permissions.LicenzaWrite),
-            PolicyDescription = "LicenzaWrite",
-            PolicyPermissions = [nameof(Permissions.Licenza), nameof(Permissions.LicenzaWrite)],
-            IsDefault = true,
-            IsActive = true
-        },
-        new AuthPolicy
-        {
-            Id = 7,
-            PolicyName = nameof(Permissions.ModuloRead),
-            PolicyDescription = "ModuloRead",
-            PolicyPermissions = [nameof(Permissions.Modulo), nameof(Permissions.ModuloRead)],
-            IsDefault = true,
-            IsActive = true
-        },
-        new AuthPolicy
-        {
-            Id = 8,
-            PolicyName = nameof(Permissions.ModuloWrite),
-            PolicyDescription = "ModuloWrite",
-            PolicyPermissions = [nameof(Permissions.Modulo), nameof(Permissions.ModuloWrite)],
-            IsDefault = true,
-            IsActive = true
-        },
-        new AuthPolicy
-        {
-            Id = 9,
-            PolicyName = nameof(Permissions.ProfiloRead),
-            PolicyDescription = "ProfiloRead",
-            PolicyPermissions = [nameof(Permissions.Profilo), nameof(Permissions.ProfiloRead)],
-            IsDefault = true,
-            IsActive = true
-        },
-        new AuthPolicy
-        {
-            Id = 10,
-            PolicyName = nameof(Permissions.ProfiloWrite),
-            PolicyDescription = "ProfiloWrite",
-            PolicyPermissions = [nameof(Permissions.Profilo), nameof(Permissions.ProfiloWrite)],
-            IsDefault = true,
-            IsActive = true
-        },
-        new AuthPolicy
-        {
-            Id = 11,
-            PolicyName = nameof(Permissions.RuoloRead),
-            PolicyDescription = "RuoloRead",
-            PolicyPermissions = [nameof(Permissions.Ruolo), nameof(Permissions.RuoloRead)],
-            IsDefault = true,
-            IsActive = true
-        },
-        new AuthPolicy
-        {
-            Id = 12,
-            PolicyName = nameof(Permissions.RuoloWrite),
-            PolicyDescription = "RuoloWrite",
-            PolicyPermissions = [nameof(Permissions.Ruolo), nameof(Permissions.RuoloWrite)],
-            IsDefault = true,
-            IsActive = true
-        });
+            new()
+            {
+                Id = 1,
+                PolicyName = nameof(Permissions.AuthPolicyRead),
+                PolicyDescription = nameof(Permissions.AuthPolicyRead),
+                PolicyPermissions = [nameof(Permissions.AuthPolicy), nameof(Permissions.AuthPolicyRead)],
+                IsDefault = true,
+                IsActive = true
+            },
+            new()
+            {
+                Id = 2,
+                PolicyName = nameof(Permissions.AuthPolicyWrite),
+                PolicyDescription = nameof(Permissions.AuthPolicyWrite),
+                PolicyPermissions = [nameof(Permissions.AuthPolicy), nameof(Permissions.AuthPolicyWrite)],
+                IsDefault = true,
+                IsActive = true
+            },
+            new()
+            {
+                Id = 3,
+                PolicyName = nameof(Permissions.ClaimRead),
+                PolicyDescription = nameof(Permissions.ClaimRead),
+                PolicyPermissions = [nameof(Permissions.Claim), nameof(Permissions.ClaimRead)],
+                IsDefault = true,
+                IsActive = true
+            },
+            new()
+            {
+                Id = 4,
+                PolicyName = nameof(Permissions.ClaimWrite),
+                PolicyDescription = nameof(Permissions.ClaimWrite),
+                PolicyPermissions = [nameof(Permissions.Claim), nameof(Permissions.ClaimWrite)],
+                IsDefault = true,
+                IsActive = true
+            },
+            new()
+            {
+                Id = 5,
+                PolicyName = nameof(Permissions.LicenzaRead),
+                PolicyDescription = nameof(Permissions.LicenzaRead),
+                PolicyPermissions = [nameof(Permissions.Licenza), nameof(Permissions.LicenzaRead)],
+                IsDefault = true,
+                IsActive = true
+            },
+            new()
+            {
+                Id = 6,
+                PolicyName = nameof(Permissions.LicenzaWrite),
+                PolicyDescription = nameof(Permissions.LicenzaWrite),
+                PolicyPermissions = [nameof(Permissions.Licenza), nameof(Permissions.LicenzaWrite)],
+                IsDefault = true,
+                IsActive = true
+            },
+            new()
+            {
+                Id = 7,
+                PolicyName = nameof(Permissions.ModuloRead),
+                PolicyDescription = nameof(Permissions.ModuloRead),
+                PolicyPermissions = [nameof(Permissions.Modulo), nameof(Permissions.ModuloRead)],
+                IsDefault = true,
+                IsActive = true
+            },
+            new()
+            {
+                Id = 8,
+                PolicyName = nameof(Permissions.ModuloWrite),
+                PolicyDescription = nameof(Permissions.ModuloWrite),
+                PolicyPermissions = [nameof(Permissions.Modulo), nameof(Permissions.ModuloWrite)],
+                IsDefault = true,
+                IsActive = true
+            },
+            new()
+            {
+                Id = 9,
+                PolicyName = nameof(Permissions.ProfiloRead),
+                PolicyDescription = nameof(Permissions.ProfiloRead),
+                PolicyPermissions = [nameof(Permissions.Profilo), nameof(Permissions.ProfiloRead)],
+                IsDefault = true,
+                IsActive = true
+            },
+            new()
+            {
+                Id = 10,
+                PolicyName = nameof(Permissions.ProfiloWrite),
+                PolicyDescription = nameof(Permissions.ProfiloWrite),
+                PolicyPermissions = [nameof(Permissions.Profilo), nameof(Permissions.ProfiloWrite)],
+                IsDefault = true,
+                IsActive = true
+            },
+            new()
+            {
+                Id = 11,
+                PolicyName = nameof(Permissions.RuoloRead),
+                PolicyDescription = nameof(Permissions.RuoloRead),
+                PolicyPermissions = [nameof(Permissions.Ruolo), nameof(Permissions.RuoloRead)],
+                IsDefault = true,
+                IsActive = true
+            },
+            new()
+            {
+                Id = 12,
+                PolicyName = nameof(Permissions.RuoloWrite),
+                PolicyDescription = nameof(Permissions.RuoloWrite),
+                PolicyPermissions = [nameof(Permissions.Ruolo), nameof(Permissions.RuoloWrite)],
+                IsDefault = true,
+                IsActive = true
+            }
+        };
+
+        builder.HasData(policies);
     }
 }
