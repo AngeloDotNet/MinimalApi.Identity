4ec4d5793a2b046eaf8755578b233c4048b3109d
Angelo Pirola <angelo9871@gmail.com>
2025-10-15 12:53:10 +0200

    Add EndpointsHandler for role management operations
    Introduced a new `EndpointsHandler` class in the
    `MinimalApi.Identity.RolesManager.Extensions` namespace to handle
    role-related operations. Added methods for retrieving, creating,
    assigning, revoking, and deleting roles using `IRoleService`.
    
    Included necessary `using` directives for dependencies such as
    `Microsoft.AspNetCore.Http` and `Microsoft.AspNetCore.Mvc`.
    Each method leverages dependency injection and returns standardized
    `IResult` responses.

---
+++ src/MinimalApi.Identity.RolesManager/Extensions/EndpointsHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MinimalApi.Identity.RolesManager.Models;
+using MinimalApi.Identity.RolesManager.Services;
+
+namespace MinimalApi.Identity.RolesManager.Extensions;
+
+public static class EndpointsHandler
+{
+    public static async Task<IResult> GetAllHandlerAsync([FromServices] IRoleService roleService, HttpContext httpContext)
+        => Results.Ok(await roleService.GetAllRolesAsync(httpContext.RequestAborted));
+
+    public static async Task<IResult> CreateRoleHandlerAsync([FromServices] IRoleService roleService, [FromBody] CreateRoleModel inputModel,
+        HttpContext httpContext) => Results.Ok(await roleService.CreateRoleAsync(inputModel, httpContext.RequestAborted));
+
+    public static async Task<IResult> AssignRoleAsync([FromServices] IRoleService roleService, [FromBody] AssignRoleModel inputModel,
+        HttpContext httpContext) => Results.Ok(await roleService.AssignRoleAsync(inputModel, httpContext.RequestAborted));
+
+    public static async Task<IResult> RevokeRoleAsync([FromServices] IRoleService roleService, [FromBody] RevokeRoleModel inputModel,
+        HttpContext httpContext) => Results.Ok(await roleService.RevokeRoleAsync(inputModel, httpContext.RequestAborted));
+
+    public static async Task<IResult> DeleteRoleAsync([FromServices] IRoleService roleService, [FromBody] DeleteRoleModel inputModel,
+        HttpContext httpContext) => Results.Ok(await roleService.DeleteRoleAsync(inputModel, httpContext.RequestAborted));
+}
