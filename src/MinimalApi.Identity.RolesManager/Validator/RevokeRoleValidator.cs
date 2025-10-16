5d1f419f65bfdd1f76796c0cf4695d0464e2a913
Angelo Pirola <angelo9871@gmail.com>
2025-10-16 11:55:04 +0200

    Add RevokeRoleValidator for role revocation validation
    Introduced the `RevokeRoleValidator` class in the
    `MinimalApi.Identity.RolesManager.Validator` namespace to validate
    `RevokeRoleModel` inputs.
    
    The validator ensures:
    - `Username` is required with a custom error message.
    - `Role` is required with a custom error message.
    
    Added necessary `using` directives for `FluentValidation` and
    `MinimalApi.Identity.RolesManager.Models`.

---
+++ src/MinimalApi.Identity.RolesManager/Validator/RevokeRoleValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MinimalApi.Identity.RolesManager.Models;
+
+namespace MinimalApi.Identity.RolesManager.Validator;
+
+public class RevokeRoleValidator : AbstractValidator<RevokeRoleModel>
+{
+    public RevokeRoleValidator()
+    {
+        RuleFor(x => x.Username)
+            .NotEmpty().WithMessage("Username is required");
+
+        RuleFor(x => x.Role)
+            .NotEmpty().WithMessage("Role is required");
+    }
+}
