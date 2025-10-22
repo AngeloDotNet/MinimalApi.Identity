f4393b82dbff4091d66bef29eef288e478517681
Angelo Pirola <angelo9871@gmail.com>
2025-10-22 12:30:41 +0200

    Remove inline comments in MinioOptions properties
    The inline comments describing the purpose of each property in the `MinioOptions` class have been removed. The properties `Endpoint`, `AccessKey`, `SecretKey`, `BucketName`, and `LogObjectKey` remain unchanged in terms of their data types and initial values.

--- src/MinimalApi.Identity.Shared/Options/MinioOptions.cs
+++ src/MinimalApi.Identity.Shared/Options/MinioOptions.cs
@@ -2,9 +2,9 @@
 
 public class MinioOptions
 {
-    public string Endpoint { get; set; } = null!; // MinIO server endpoint: http://127.0.0.1:9000
-    public string AccessKey { get; set; } = null!; // access credentials
-    public string SecretKey { get; set; } = null!; // access credentials
-    public string BucketName { get; set; } = null!; // bucket name: logs
-    public string LogObjectKey { get; set; } = null!; // object prefix: serilog-demo.json
+    public string Endpoint { get; set; } = null!;
+    public string AccessKey { get; set; } = null!;
+    public string SecretKey { get; set; } = null!;
+    public string BucketName { get; set; } = null!;
+    public string LogObjectKey { get; set; } = null!;
 }
