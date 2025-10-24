8c74ed6803764112ba5f568ca899d8f32a0c0b39
Angelo Pirola <angelo9871@gmail.com>
2025-10-24 09:47:55 +0200

    Refactor AuthManagerRegistrationService method
    Refactored the `AuthManagerRegistrationService` method in the
    `AuthExtensions` class to use expression-bodied member syntax
    for improved readability and reduced boilerplate. The method
    functionality remains unchanged, and it still registers the
    `AuthenticationStartupTask` hosted service.

--- src/MinimalApi.Identity.AuthManager/DependencyInjection/AuthExtensions.cs
+++ src/MinimalApi.Identity.AuthManager/DependencyInjection/AuthExtensions.cs
@@ -6,10 +6,5 @@ namespace MinimalApi.Identity.AuthManager.DependencyInjection;
 public static class AuthExtensions
 {
     public static IServiceCollection AuthManagerRegistrationService(this IServiceCollection services)
-    {
-        services
-            .AddHostedService<AuthenticationStartupTask>();
-
-        return services;
-    }
+        => services.AddHostedService<AuthenticationStartupTask>();
 }
