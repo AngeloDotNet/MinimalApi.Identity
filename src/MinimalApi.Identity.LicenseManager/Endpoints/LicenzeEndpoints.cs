a3d2e771e02b451c87584cea590a9e335c23e31e
Angelo Pirola <angelo9871@gmail.com>
2025-11-20 10:47:27 +0100

    Simplify OpenAPI metadata configuration
    Refactored endpoint definitions to simplify OpenAPI metadata:
    - Removed `using Microsoft.OpenApi.Models` as it is no longer needed.
    - Replaced detailed `.WithOpenApi` configurations with `.WithOpenApi()` calls.
    - Replaced `.ProducesDefaultProblem` with `.ProducesProblem` for better error descriptions using constants.
    - Moved descriptions and summaries to `.WithDescription` and `.WithSummary` for clarity.
    - Enhanced `.Produces` calls with additional context via `.WithDescription`.
    - Retained validation and authorization configurations.
    - Preserved commented-out `.WithOpenApi` configurations for reference.
    
    These changes improve readability, maintainability, and consistency.

--- src/MinimalApi.Identity.LicenseManager/Endpoints/LicenzeEndpoints.cs
+++ src/MinimalApi.Identity.LicenseManager/Endpoints/LicenzeEndpoints.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.OpenApi.Models;
 using MinimalApi.Identity.Core.Configurations;
 using MinimalApi.Identity.Core.DependencyInjection;
 using MinimalApi.Identity.Core.Enums;
@@ -19,95 +18,120 @@ public static class LicenseEndpoints
     {
         var apiGroup = endpoints
             .MapGroup(EndpointGenerator.EndpointsLicenseGroup)
-            .WithOpenApi(opt =>
-            {
-                opt.Tags = [new OpenApiTag { Name = EndpointGenerator.EndpointsLicenseTag }];
-                return opt;
-            });
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Tags = [new OpenApiTag { Name = EndpointGenerator.EndpointsLicenseTag }];
+            //    return opt;
+            //})
+            .WithOpenApi();
 
         apiGroup.MapGet(EndpointGenerator.EndpointsStringEmpty, EndpointsHandler.GetAllHandlerAsync)
-            .Produces<Ok<List<LicenseResponseModel>>>(StatusCodes.Status200OK)
-            .ProducesDefaultProblem(StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound)
+            .Produces<Ok<List<LicenseResponseModel>>>(StatusCodes.Status200OK).WithDescription("Get all licenses")
+            //.ProducesDefaultProblem(StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound)
+            .ProducesProblem(StatusCodes.Status401Unauthorized, ConstantsConfiguration.Unauthorized)
+            .ProducesProblem(StatusCodes.Status404NotFound, ConstantsConfiguration.NotFound)
             .RequireAuthorization(nameof(Permissions.LicenzaRead))
-            .WithOpenApi(opt =>
-            {
-                opt.Description = "Get all licenses";
-                opt.Summary = "Get all licenses";
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Description = "Get all licenses";
+            //    opt.Summary = "Get all licenses";
 
-                opt.Response(StatusCodes.Status200OK).Description = "Licenses retrieved successfully";
-                opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
-                opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
+            //    opt.Response(StatusCodes.Status200OK).Description = "Licenses retrieved successfully";
+            //    opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
+            //    opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
 
-                return opt;
-            });
+            //    return opt;
+            //})
+            .WithDescription("Get all licenses")
+            .WithSummary("Get all licenses");
 
         apiGroup.MapPost(LicenseExtensions.EndpointsCreateLicense, EndpointsHandler.CreateHandlerAsync)
-            .Produces<Ok<string>>(StatusCodes.Status200OK)
-            .ProducesDefaultProblem(StatusCodes.Status401Unauthorized, StatusCodes.Status422UnprocessableEntity)
+            .Produces<Ok<string>>(StatusCodes.Status200OK).WithDescription("Create a new license")
+            //.ProducesDefaultProblem(StatusCodes.Status401Unauthorized, StatusCodes.Status422UnprocessableEntity)
+            .ProducesProblem(StatusCodes.Status401Unauthorized, ConstantsConfiguration.Unauthorized)
+            .ProducesProblem(StatusCodes.Status422UnprocessableEntity).WithDescription("Validation error")
             .RequireAuthorization(nameof(Permissions.LicenzaWrite))
             .WithValidation<CreateLicenseModel>()
-            .WithOpenApi(opt =>
-            {
-                opt.Description = "Create a new license";
-                opt.Summary = "Create a new license";
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Description = "Create a new license";
+            //    opt.Summary = "Create a new license";
 
-                opt.Response(StatusCodes.Status200OK).Description = "License created successfully";
-                opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
+            //    opt.Response(StatusCodes.Status200OK).Description = "License created successfully";
+            //    opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
 
-                return opt;
-            });
+            //    return opt;
+            //})
+            .WithDescription("Create a new license")
+            .WithSummary("Create a new license");
 
         apiGroup.MapPost(LicenseExtensions.EndpointsAssignLicense, EndpointsHandler.AssignHandlerAsync)
-            .Produces<Ok<string>>(StatusCodes.Status200OK)
-            .ProducesDefaultProblem(StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound, StatusCodes.Status422UnprocessableEntity)
+            .Produces<Ok<string>>(StatusCodes.Status200OK).WithDescription("Assign a license to a user")
+            //.ProducesDefaultProblem(StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound, StatusCodes.Status422UnprocessableEntity)
+            .ProducesProblem(StatusCodes.Status401Unauthorized, ConstantsConfiguration.Unauthorized)
+            .ProducesProblem(StatusCodes.Status404NotFound, ConstantsConfiguration.NotFound)
+            .ProducesProblem(StatusCodes.Status422UnprocessableEntity).WithDescription("Validation error")
             .RequireAuthorization(nameof(Permissions.LicenzaWrite))
             .WithValidation<AssignLicenseModel>()
-            .WithOpenApi(opt =>
-            {
-                opt.Description = "Assign a license to a user";
-                opt.Summary = "Assign a license to a user";
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Description = "Assign a license to a user";
+            //    opt.Summary = "Assign a license to a user";
 
-                opt.Response(StatusCodes.Status200OK).Description = "License assigned successfully";
-                opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
-                opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
+            //    opt.Response(StatusCodes.Status200OK).Description = "License assigned successfully";
+            //    opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
+            //    opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
 
-                return opt;
-            });
+            //    return opt;
+            //})
+            .WithDescription("Assign a license to a user")
+            .WithSummary("Assign a license to a user");
 
         apiGroup.MapDelete(LicenseExtensions.EndpointsRevokeLicense, EndpointsHandler.RevokeHandlerAsync)
-            .Produces<Ok<string>>(StatusCodes.Status200OK)
-            .ProducesDefaultProblem(StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound, StatusCodes.Status422UnprocessableEntity)
+            .Produces<Ok<string>>(StatusCodes.Status200OK).WithDescription("Revoke a license from a user")
+            //.ProducesDefaultProblem(StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound, StatusCodes.Status422UnprocessableEntity)
+            .ProducesProblem(StatusCodes.Status401Unauthorized, ConstantsConfiguration.Unauthorized)
+            .ProducesProblem(StatusCodes.Status404NotFound, ConstantsConfiguration.NotFound)
+            .ProducesProblem(StatusCodes.Status422UnprocessableEntity).WithDescription("Validation error")
             .RequireAuthorization(nameof(Permissions.LicenzaWrite))
             .WithValidation<RevokeLicenseModel>()
-            .WithOpenApi(opt =>
-            {
-                opt.Description = "Revoke a license from a user";
-                opt.Summary = "Revoke a license from a user";
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Description = "Revoke a license from a user";
+            //    opt.Summary = "Revoke a license from a user";
 
-                opt.Response(StatusCodes.Status200OK).Description = "License revoked successfully";
-                opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
-                opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
+            //    opt.Response(StatusCodes.Status200OK).Description = "License revoked successfully";
+            //    opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
+            //    opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
 
-                return opt;
-            });
+            //    return opt;
+            //});
+            .WithDescription("Revoke a license from a user")
+            .WithSummary("Revoke a license from a user");
 
         apiGroup.MapDelete(LicenseExtensions.EndpointsDeleteLicense, EndpointsHandler.DeleteHandlerAsync)
-            .Produces<string>(StatusCodes.Status200OK)
-            .ProducesDefaultProblem(StatusCodes.Status400BadRequest, StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound, StatusCodes.Status422UnprocessableEntity)
+            .Produces<string>(StatusCodes.Status200OK).WithDescription("Delete license")
+            //.ProducesDefaultProblem(StatusCodes.Status400BadRequest, StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound, StatusCodes.Status422UnprocessableEntity)
+            .ProducesProblem(StatusCodes.Status400BadRequest, ConstantsConfiguration.BadRequest)
+            .ProducesProblem(StatusCodes.Status401Unauthorized, ConstantsConfiguration.Unauthorized)
+            .ProducesProblem(StatusCodes.Status404NotFound, ConstantsConfiguration.NotFound)
+            .ProducesProblem(StatusCodes.Status422UnprocessableEntity).WithDescription("Validation error")
             .RequireAuthorization(nameof(Permissions.LicenzaWrite))
             .WithValidation<DeleteLicenseModel>()
-            .WithOpenApi(opt =>
-            {
-                opt.Summary = "Delete license";
-                opt.Description = "Delete license";
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Summary = "Delete license";
+            //    opt.Description = "Delete license";
 
-                opt.Response(StatusCodes.Status200OK).Description = "License deleted successfully";
-                opt.Response(StatusCodes.Status400BadRequest).Description = ConstantsConfiguration.BadRequest;
-                opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
-                opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
+            //    opt.Response(StatusCodes.Status200OK).Description = "License deleted successfully";
+            //    opt.Response(StatusCodes.Status400BadRequest).Description = ConstantsConfiguration.BadRequest;
+            //    opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
+            //    opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
 
-                return opt;
-            });
+            //    return opt;
+            //});
+            .WithDescription("Delete license")
+            .WithSummary("Delete license");
 
         return apiGroup;
     }
