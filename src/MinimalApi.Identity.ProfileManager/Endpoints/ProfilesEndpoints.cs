b779ca2397076c09c496ac6440c6947411e1ec6c
Angelo Pirola <angelo9871@gmail.com>
2025-11-20 10:46:33 +0100

    Refactor ProfilesEndpoints for clarity and OpenAPI cleanup
    Simplified OpenAPI configuration by replacing explicit `WithOpenApi` tag setups with a streamlined `.WithOpenApi()` method.
    
    Enhanced endpoint documentation by adding `.WithDescription()` and `.WithSummary()` for better clarity. Replaced `ProducesDefaultProblem` with more specific `ProducesProblem` calls, including detailed status code descriptions.
    
    Removed unnecessary `using Microsoft.OpenApi.Models` directive and performed general cleanup to improve code readability and maintainability. Retained commented-out code for reference.

--- src/MinimalApi.Identity.ProfileManager/Endpoints/ProfilesEndpoints.cs
+++ src/MinimalApi.Identity.ProfileManager/Endpoints/ProfilesEndpoints.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.OpenApi.Models;
 using MinimalApi.Identity.Core.Configurations;
 using MinimalApi.Identity.Core.DependencyInjection;
 using MinimalApi.Identity.Core.Enums;
@@ -18,76 +17,96 @@ public static class ProfilesEndpoints
     {
         var apiGroup = endpoints
             .MapGroup(EndpointGenerator.EndpointsProfilesGroup)
-            .WithOpenApi(opt =>
-            {
-                opt.Tags = [new OpenApiTag { Name = EndpointGenerator.EndpointsProfilesTag }];
-                return opt;
-            });
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Tags = [new OpenApiTag { Name = EndpointGenerator.EndpointsProfilesTag }];
+            //    return opt;
+            //});
+            .WithOpenApi();
 
         apiGroup.MapGet(EndpointGenerator.EndpointsStringEmpty, EndpointsHandler.GetAllHandlerAsync)
-            .Produces<List<UserProfileModel>>(StatusCodes.Status200OK)
-            .ProducesDefaultProblem(StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound)
+            .Produces<List<UserProfileModel>>(StatusCodes.Status200OK).WithDescription("List of users profiles")
+            //.ProducesDefaultProblem(StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound)
+            .ProducesProblem(StatusCodes.Status401Unauthorized, ConstantsConfiguration.Unauthorized)
+            .ProducesProblem(StatusCodes.Status404NotFound, ConstantsConfiguration.NotFound)
             .RequireAuthorization(nameof(Permissions.ProfiloRead))
-            .WithOpenApi(opt =>
-            {
-                opt.Summary = "Get all profiles";
-                opt.Description = "Get all profiles";
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Summary = "Get all profiles";
+            //    opt.Description = "Get all profiles";
 
-                opt.Response(StatusCodes.Status200OK).Description = "List of users profiles";
-                opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
-                opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
+            //    opt.Response(StatusCodes.Status200OK).Description = "List of users profiles";
+            //    opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
+            //    opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
 
-                return opt;
-            });
+            //    return opt;
+            //});
+            .WithDescription("Get all profiles")
+            .WithSummary("Get all profiles");
 
         apiGroup.MapGet(ProfileExtensions.EndpointsGetProfile, EndpointsHandler.GetProfileHandlerAsync)
-        .Produces<UserProfileModel>(StatusCodes.Status200OK)
-        .ProducesDefaultProblem(StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound)
+        .Produces<UserProfileModel>(StatusCodes.Status200OK).WithDescription("User profile retrieved successfully")
+        //.ProducesDefaultProblem(StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status401Unauthorized, ConstantsConfiguration.Unauthorized)
+        .ProducesProblem(StatusCodes.Status404NotFound, ConstantsConfiguration.NotFound)
         .RequireAuthorization(nameof(Permissions.ProfiloRead))
-        .WithOpenApi(opt =>
-        {
-            opt.Summary = "Get user profile";
-            opt.Description = "Get user profile by username";
+        //.WithOpenApi(opt =>
+        //{
+        //    opt.Summary = "Get user profile";
+        //    opt.Description = "Get user profile by username";
 
-            opt.Response(StatusCodes.Status200OK).Description = "User profile retrieved successfully";
-            opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
-            opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
-            return opt;
-        });
+        //    opt.Response(StatusCodes.Status200OK).Description = "User profile retrieved successfully";
+        //    opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
+        //    opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
+        //    return opt;
+        //});
+        .WithDescription("Get user profile")
+        .WithSummary("Get user profile");
 
         apiGroup.MapPost(ProfileExtensions.EndpointsChangeEnableProfile, EndpointsHandler.ChangeUserStatusAsync)
-            .Produces<UserProfileModel>(StatusCodes.Status200OK)
-            .ProducesDefaultProblem(StatusCodes.Status400BadRequest, StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound)
+            .Produces<UserProfileModel>(StatusCodes.Status200OK).WithDescription("User profile enablement changed successfully")
+            //.ProducesDefaultProblem(StatusCodes.Status400BadRequest, StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound)
+            .ProducesProblem(StatusCodes.Status400BadRequest).WithDescription(ConstantsConfiguration.BadRequest)
+            .ProducesProblem(StatusCodes.Status401Unauthorized, ConstantsConfiguration.Unauthorized)
+            .ProducesProblem(StatusCodes.Status404NotFound, ConstantsConfiguration.NotFound)
             .RequireAuthorization(nameof(Permissions.ProfiloRead))
-            .WithOpenApi(opt =>
-            {
-                opt.Summary = "Edit user profile enablement";
-                opt.Description = "Edit user profile enablement";
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Summary = "Edit user profile enablement";
+            //    opt.Description = "Edit user profile enablement";
 
-                opt.Response(StatusCodes.Status200OK).Description = "User profile retrieved successfully";
-                opt.Response(StatusCodes.Status400BadRequest).Description = "Bad request";
-                opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
-                opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
-                return opt;
-            });
+            //    opt.Response(StatusCodes.Status200OK).Description = "User profile retrieved successfully";
+            //    opt.Response(StatusCodes.Status400BadRequest).Description = "Bad request";
+            //    opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
+            //    opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
+            //    return opt;
+            //});
+            .WithDescription("Edit user profile enablement")
+            .WithSummary("Edit user profile enablement");
 
         apiGroup.MapPut(ProfileExtensions.EndpointsEditProfile, EndpointsHandler.EditProfileAsync)
-            .Produces<string>(StatusCodes.Status200OK)
-            .ProducesDefaultProblem(StatusCodes.Status400BadRequest, StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound, StatusCodes.Status422UnprocessableEntity)
+            .Produces<string>(StatusCodes.Status200OK).WithDescription("User profile updated successfully")
+            //.ProducesDefaultProblem(StatusCodes.Status400BadRequest, StatusCodes.Status401Unauthorized, StatusCodes.Status404NotFound, StatusCodes.Status422UnprocessableEntity)
+            .ProducesProblem(StatusCodes.Status400BadRequest).WithDescription(ConstantsConfiguration.BadRequest)
+            .ProducesProblem(StatusCodes.Status401Unauthorized, ConstantsConfiguration.Unauthorized)
+            .ProducesProblem(StatusCodes.Status404NotFound, ConstantsConfiguration.NotFound)
+            .ProducesProblem(StatusCodes.Status422UnprocessableEntity).WithDescription("Validation error")
             .RequireAuthorization(nameof(Permissions.ProfiloWrite))
             .WithValidation<EditUserProfileModel>()
-            .WithOpenApi(opt =>
-            {
-                opt.Summary = "Update user profile";
-                opt.Description = "Update user profile by username";
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Summary = "Update user profile";
+            //    opt.Description = "Update user profile by username";
 
-                opt.Response(StatusCodes.Status200OK).Description = "User profile updated successfully";
-                opt.Response(StatusCodes.Status400BadRequest).Description = "Bad request";
-                opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
-                opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
+            //    opt.Response(StatusCodes.Status200OK).Description = "User profile updated successfully";
+            //    opt.Response(StatusCodes.Status400BadRequest).Description = "Bad request";
+            //    opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
+            //    opt.Response(StatusCodes.Status404NotFound).Description = ConstantsConfiguration.NotFound;
 
-                return opt;
-            });
+            //    return opt;
+            //});
+            .WithDescription("Update user profile")
+            .WithSummary("Update user profile");
 
         return apiGroup;
     }
