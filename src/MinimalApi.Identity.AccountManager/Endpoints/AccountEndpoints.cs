966d69c691e1e30d19b3ca3c78b42ac25a9265d8
Angelo Pirola <angelo9871@gmail.com>
2025-11-20 10:47:39 +0100

    Simplify OpenAPI config and enforce authorization
    Refactored OpenAPI configurations by replacing detailed metadata definitions with concise `.WithOpenApi()` calls. Removed unused `Microsoft.OpenApi.Models` import. Enhanced response metadata using `.WithDescription()` and `.WithSummary()` for better clarity.
    
    Enforced authorization at the group level with `.RequireAuthorization()`. Replaced `ProducesDefaultProblem` with more specific `ProducesProblem` calls for individual status codes. Adjusted access control for endpoints to ensure consistency.
    
    These changes improve code readability, maintainability, and ensure proper access control while streamlining OpenAPI documentation generation.

--- src/MinimalApi.Identity.AccountManager/Endpoints/AccountEndpoints.cs
+++ src/MinimalApi.Identity.AccountManager/Endpoints/AccountEndpoints.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.OpenApi.Models;
 using MinimalApi.Identity.AccountManager.DependencyInjection;
 using MinimalApi.Identity.AccountManager.Extensions;
 using MinimalApi.Identity.AccountManager.Models;
@@ -18,58 +17,71 @@ public static class AccountEndpoints
     {
         var apiGroup = endpoints
             .MapGroup(EndpointGenerator.EndpointsAccountGroup)
-            .WithOpenApi(opt =>
-            {
-                opt.Tags = [new OpenApiTag { Name = EndpointGenerator.EndpointsAccountTag }];
-                return opt;
-            });
+            .RequireAuthorization()
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Tags = [new OpenApiTag { Name = EndpointGenerator.EndpointsAccountTag }];
+            //    return opt;
+            //})
+            .WithOpenApi();
 
         apiGroup.MapGet(EndpointGenerator.EndpointsConfirmEmail, EndpointsHandler.ConfirmEmailAsync)
-            .Produces<Ok<string>>(StatusCodes.Status200OK)
-            .ProducesDefaultProblem(StatusCodes.Status400BadRequest)
+            .Produces<Ok<string>>(StatusCodes.Status200OK).WithDescription("Email address confirmed successfully")
+            .ProducesProblem(StatusCodes.Status400BadRequest).WithDescription(ConstantsConfiguration.BadRequest)
+            //.ProducesDefaultProblem(StatusCodes.Status400BadRequest)
             .AllowAnonymous()
-            .WithOpenApi(opt =>
-            {
-                opt.Description = "Confirm email address";
-                opt.Summary = "Confirm email address";
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Description = "Confirm email address";
+            //    opt.Summary = "Confirm email address";
 
-                opt.Response(StatusCodes.Status200OK).Description = "Email address confirmed successfully";
-                opt.Response(StatusCodes.Status400BadRequest).Description = ConstantsConfiguration.BadRequest;
+            //    opt.Response(StatusCodes.Status200OK).Description = "Email address confirmed successfully";
+            //    opt.Response(StatusCodes.Status400BadRequest).Description = ConstantsConfiguration.BadRequest;
 
-                return opt;
-            });
+            //    return opt;
+            //})
+            .WithDescription("Confirm email address")
+            .WithSummary("Confirm email address");
 
         apiGroup.MapPost(AccountExtensions.EndpointChangeEmail, EndpointsHandler.ChangeEmailAsync)
-            .Produces<Ok<string>>(StatusCodes.Status200OK)
-            .ProducesDefaultProblem(StatusCodes.Status400BadRequest, StatusCodes.Status401Unauthorized, StatusCodes.Status422UnprocessableEntity)
-            .RequireAuthorization()
+            .Produces<Ok<string>>(StatusCodes.Status200OK).WithDescription("Email address changed successfully")
+            //.ProducesDefaultProblem(StatusCodes.Status400BadRequest, StatusCodes.Status401Unauthorized, StatusCodes.Status422UnprocessableEntity)
+            .ProducesProblem(StatusCodes.Status400BadRequest).WithDescription(ConstantsConfiguration.BadRequest)
+            .ProducesProblem(StatusCodes.Status401Unauthorized).WithDescription(ConstantsConfiguration.Unauthorized)
+            .ProducesProblem(StatusCodes.Status422UnprocessableEntity).WithDescription("Validation error")
+            //.RequireAuthorization()
             .WithValidation<ChangeEmailModel>()
-            .WithOpenApi(opt =>
-            {
-                opt.Description = "Change email address";
-                opt.Summary = "Change email address";
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Description = "Change email address";
+            //    opt.Summary = "Change email address";
 
-                opt.Response(StatusCodes.Status200OK).Description = "Email address changed successfully";
-                opt.Response(StatusCodes.Status400BadRequest).Description = ConstantsConfiguration.BadRequest;
-                opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
+            //    opt.Response(StatusCodes.Status200OK).Description = "Email address changed successfully";
+            //    opt.Response(StatusCodes.Status400BadRequest).Description = ConstantsConfiguration.BadRequest;
+            //    opt.Response(StatusCodes.Status401Unauthorized).Description = ConstantsConfiguration.Unauthorized;
 
-                return opt;
-            });
+            //    return opt;
+            //})
+            .WithDescription("Change email address")
+            .WithSummary("Change email address");
 
         apiGroup.MapGet(EndpointGenerator.EndpointsConfirmEmailChange, EndpointsHandler.ConfirmEmailChangeAsync)
-            .Produces<Ok<string>>(StatusCodes.Status200OK)
-            .ProducesDefaultProblem(StatusCodes.Status400BadRequest)
+            .Produces<Ok<string>>(StatusCodes.Status200OK).WithDescription("Email address change confirmed successfully")
+            .ProducesProblem(StatusCodes.Status400BadRequest).WithDescription(ConstantsConfiguration.BadRequest)
+            //.ProducesDefaultProblem(StatusCodes.Status400BadRequest)
             .AllowAnonymous()
-            .WithOpenApi(opt =>
-            {
-                opt.Description = "Confirm email address change";
-                opt.Summary = "Confirm email address change";
+            //.WithOpenApi(opt =>
+            //{
+            //    opt.Description = "Confirm email address change";
+            //    opt.Summary = "Confirm email address change";
 
-                opt.Response(StatusCodes.Status200OK).Description = "Email address change confirmed successfully";
-                opt.Response(StatusCodes.Status400BadRequest).Description = ConstantsConfiguration.BadRequest;
+            //    opt.Response(StatusCodes.Status200OK).Description = "Email address change confirmed successfully";
+            //    opt.Response(StatusCodes.Status400BadRequest).Description = ConstantsConfiguration.BadRequest;
 
-                return opt;
-            });
+            //    return opt;
+            //})
+            .WithDescription("Confirm email address change")
+            .WithSummary("Confirm email address change");
 
         return apiGroup;
     }
