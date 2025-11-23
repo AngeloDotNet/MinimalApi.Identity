8f861f4f2a35cba37e1c0605763fcd00e1345df4
Angelo Pirola <angelo9871@gmail.com>
2025-11-23 18:29:47 +0100

    Add ValidationOptions class for validation configuration
    Introduce the `ValidationOptions` class in the `MinimalApi.Identity.Core.Options` namespace to configure validation-related options.
    
    - Add `ErrorResponseFormat` property to specify error response formats.
    - Add `ValidationErrorTitleMessageFactory` property to generate custom error titles for validation errors.
    - Add `using` directives for `Microsoft.AspNetCore.Http` and `MinimalApi.Identity.Shared.Results.AspNetCore.Http` to support the new functionality.

---
+++ src/MinimalApi.Identity.Core/Options/ValidationOptions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using MinimalApi.Identity.Shared.Results.AspNetCore.Http;
+
+namespace MinimalApi.Identity.Core.Options;
+
+public class ValidationOptions
+{
+    public ErrorResponseFormat ErrorResponseFormat { get; set; }
+    public Func<EndpointFilterInvocationContext, IDictionary<string, string[]>, string>? ValidationErrorTitleMessageFactory { get; set; }
+}
