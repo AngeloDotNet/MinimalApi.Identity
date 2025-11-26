00643b6713e094439d0d30465fb913e10bfd6564
Angelo Pirola <angelo9871@gmail.com>
2025-11-27 00:01:06 +0100

    Refactor Swagger filters and add feature-specific filters
    Refactored `SwaggerFeatureDocumentFilter` to improve clarity and maintainability. Updated `using` directives to include necessary namespaces.
    
    Introduced two new feature-specific filters:
    - `LicensesManagementSwaggerFilter` for handling license-related endpoints.
    - `ModulesManagementSwaggerFilter` for handling module-related endpoints.
    
    Commented out the original generic implementation of `SwaggerFeatureDocumentFilter` in favor of the new, more specific filters.

--- src/MinimalApi.Identity.Core/Filters/SwaggerFeatureDocumentFilter.cs
+++ src/MinimalApi.Identity.Core/Filters/SwaggerFeatureDocumentFilter.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.OpenApi;
+﻿using Microsoft.OpenApi.Models;
 using MinimalApi.Identity.Core.Options;
+using MinimalApi.Identity.Core.Utility.Generators;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace MinimalApi.Identity.Core.Filters;
@@ -11,9 +12,8 @@ public class SwaggerFeatureDocumentFilter(FeatureFlagsOptions featureFlagsOption
     {
         if (!isFeatureEnabled(featureFlagsOptions))
         {
-            var toRemove = swaggerDoc.Paths.Keys
-                .Where(path => path.StartsWith(endpointGroup, StringComparison.OrdinalIgnoreCase))
-                .ToList();
+            var toRemove = swaggerDoc.Paths.Keys.Where(path
+                => path.StartsWith(endpointGroup, StringComparison.OrdinalIgnoreCase)).ToList();
 
             foreach (var path in toRemove)
             {
@@ -22,3 +22,75 @@ public class SwaggerFeatureDocumentFilter(FeatureFlagsOptions featureFlagsOption
         }
     }
 }
+
+//public class LicensesManagementSwaggerFilter(FeatureFlagsOptions featureFlagsOptions) : IDocumentFilter
+public class LicensesManagementSwaggerFilter(bool enabledFeatureLicense) : IDocumentFilter
+{
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        if (!enabledFeatureLicense)
+        {
+            var toRemove = swaggerDoc.Paths.Keys.Where(path
+                => path.StartsWith(EndpointGenerator.EndpointsLicenseGroup, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            foreach (var path in toRemove)
+            {
+                swaggerDoc.Paths.Remove(path);
+            }
+        }
+    }
+}
+
+//public class ModulesManagementSwaggerFilter(FeatureFlagsOptions featureFlagsOptions) : IDocumentFilter
+public class ModulesManagementSwaggerFilter(bool enabledFeatureModule) : IDocumentFilter
+{
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        if (!enabledFeatureModule)
+        {
+            var toRemove = swaggerDoc.Paths.Keys.Where(path
+                => path.StartsWith(EndpointGenerator.EndpointsModulesGroup, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            foreach (var path in toRemove)
+            {
+                swaggerDoc.Paths.Remove(path);
+            }
+        }
+    }
+}
+
+//public class SwaggerFeatureDocumentFilter : IDocumentFilter
+//{
+//    private readonly FeatureFlagsOptions featureFlagsOptions;
+//    private readonly Func<FeatureFlagsOptions, bool> isFeatureEnabled;
+//    private readonly string endpointGroup;
+
+//    public SwaggerFeatureDocumentFilter()
+//    {
+//        featureFlagsOptions = new FeatureFlagsOptions();
+//        isFeatureEnabled = _ => true;
+//        endpointGroup = string.Empty;
+//    }
+
+//    public SwaggerFeatureDocumentFilter(FeatureFlagsOptions featureFlagsOptions, Func<FeatureFlagsOptions, bool> isFeatureEnabled, string endpointGroup)
+//    {
+//        this.featureFlagsOptions = featureFlagsOptions;
+//        this.isFeatureEnabled = isFeatureEnabled;
+//        this.endpointGroup = endpointGroup;
+//    }
+
+//    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+//    {
+//        if (!isFeatureEnabled(featureFlagsOptions))
+//        {
+//            var toRemove = swaggerDoc.Paths.Keys
+//                .Where(path => path.StartsWith(endpointGroup, StringComparison.OrdinalIgnoreCase))
+//                .ToList();
+
+//            foreach (var path in toRemove)
+//            {
+//                swaggerDoc.Paths.Remove(path);
+//            }
+//        }
+//    }
+//}
