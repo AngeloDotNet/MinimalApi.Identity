66756b8b69290b47c1eca0a5dc66338e1529df77
Angelo Pirola <angelo9871@gmail.com>
2025-11-28 10:21:05 +0100

    Refactor Swagger configuration for JWT and filters
    Updated `SwaggerExtensions.cs` to enhance Swagger configuration:
    - Added required `using` directives for dependencies.
    - Introduced `AddSwaggerGenOptions` to configure API metadata,
      JWT Bearer authentication, and security requirements.
    - Re-implemented `AddSwaggerDocumentFilters` to apply dynamic
      document filters based on feature flags.
    - Removed outdated, commented-out methods.
    
    These changes improve API documentation, security, and flexibility.

--- src/MinimalApi.Identity.API/Extensions/SwaggerExtensions.cs
+++ src/MinimalApi.Identity.API/Extensions/SwaggerExtensions.cs
@@ -1,4 +1,12 @@
-﻿namespace MinimalApi.Identity.API.Extensions;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using MinimalApi.Identity.Core.Configurations;
+using MinimalApi.Identity.Core.Filters;
+using MinimalApi.Identity.Core.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace MinimalApi.Identity.API.Extensions;
 
 public static class SwaggerExtensions
 {
@@ -46,12 +54,74 @@ public static class SwaggerExtensions
     //    };
     //}
 
-    //public static void AddSwaggerDocumentFilters(this SwaggerGenOptions options, FeatureFlagsOptions featureFlagsOptions)
-    //{
-    //    options.DocumentFilter<SwaggerFeatureDocumentFilter>(featureFlagsOptions, (FeatureFlagsOptions opts)
-    //        => opts.EnabledFeatureLicense, EndpointGenerator.EndpointsLicenseGroup);
+    public static void AddSwaggerGenOptions(this SwaggerGenOptions options, FeatureFlagsOptions featureFlagsOptions)
+    {
+        var openApiInfo = new OpenApiInfo
+        {
+            Title = "Minimal API Identity",
+            Version = "v1",
+            Contact = new OpenApiContact
+            {
+                Name = "Angelo Pirola",
+                Email = "angelo@aepserver.it"
+            },
+            License = new OpenApiLicense
+            {
+                Name = "License MIT",
+                Url = new Uri(ConstantsConfiguration.LicenseMIT)
+            },
+        };
 
-    //    options.DocumentFilter<SwaggerFeatureDocumentFilter>(featureFlagsOptions, (FeatureFlagsOptions opts)
-    //        => opts.EnabledFeatureModule, EndpointGenerator.EndpointsModulesGroup);
-    //}
+        options.SwaggerDoc("v1", openApiInfo);
+        //opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Name = "Authorization",
+            Description = "JWT Authorization header using the Bearer scheme.",
+            Type = SecuritySchemeType.ApiKey,
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = JwtBearerDefaults.AuthenticationScheme
+            },
+            Scheme = JwtBearerDefaults.AuthenticationScheme
+
+            //BearerFormat = "JWT",
+            //Description = "JWT Authorization header using the Bearer scheme.",
+            //Name = "Authorization",
+            //In = ParameterLocation.Header,
+            //Type = SecuritySchemeType.ApiKey,
+            //Scheme = "Bearer"
+        });
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        //Id = "Bearer",
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+
+        options.AddSwaggerDocumentFilters(featureFlagsOptions);
+    }
+
+    public static void AddSwaggerDocumentFilters(this SwaggerGenOptions options, FeatureFlagsOptions featureFlagsOptions)
+    {
+        //options.DocumentFilter<SwaggerFeatureDocumentFilter>(featureFlagsOptions, (FeatureFlagsOptions opts)
+        //    => opts.EnabledFeatureLicense, EndpointGenerator.EndpointsLicenseGroup);
+
+        //options.DocumentFilter<SwaggerFeatureDocumentFilter>(featureFlagsOptions, (FeatureFlagsOptions opts)
+        //    => opts.EnabledFeatureModule, EndpointGenerator.EndpointsModulesGroup);
+
+        options.DocumentFilter<LicensesManagementSwaggerFilter>(featureFlagsOptions.EnabledFeatureLicense);
+        options.DocumentFilter<ModulesManagementSwaggerFilter>(featureFlagsOptions.EnabledFeatureModule);
+    }
 }
