89e6ed3c1fdc40835ccff5a606323ec419eb6b90
Angelo Pirola <angelo9871@gmail.com>
2025-12-03 09:29:27 +0100

    Refactor SwaggerSettings to use AuthSettings class
    Replaced the `IsRequiredAuth` property in `SwaggerSettings` with a new `AuthSettings` property, which encapsulates authentication-related settings (`IsRequired`, `UserName`, and `Password`). Introduced the `AuthSettings` class to centralize these properties. Commented out redundant properties in `SwaggerSettings` and added placeholders for potential future configuration in `AuthSettings`.

--- src/MinimalApi.Identity.Core/Settings/SwaggerSettings.cs
+++ src/MinimalApi.Identity.Core/Settings/SwaggerSettings.cs
@@ -3,7 +3,17 @@
 public class SwaggerSettings
 {
     public bool IsEnabled { get; set; }
-    public bool IsRequiredAuth { get; set; }
+    public AuthSettings AuthSettings { get; set; } = new AuthSettings();
+    //public bool IsRequiredAuth { get; set; }
+    //public string? UserName { get; set; }
+    //public string? Password { get; set; }
+}
+
+public class AuthSettings
+{
+    //public const string SectionName = "SwaggerSettings";
+    //public bool IsEnabled { get; set; }
+    public bool IsRequired { get; set; }
     public string? UserName { get; set; }
     public string? Password { get; set; }
 }
