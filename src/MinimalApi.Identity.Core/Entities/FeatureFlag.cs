9ce7f54dbf490b9a9b27a5c570068e91c7d2f094
Angelo Pirola <angelo9871@gmail.com>
2025-12-30 11:07:23 +0100

    Remove FeatureFlag entity and related code
    The FeatureFlag class and its properties have been deleted from FeatureFlag.cs, along with the namespace declaration. This removes the FeatureFlag entity from the codebase entirely.

--- src/MinimalApi.Identity.Core/Entities/FeatureFlag.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MinimalApi.Identity.Core.Entities;
-
-//public class FeatureFlag : BaseEntity, IEntity
-//{
-//    public string Name { get; set; } = null!;
-//    public bool IsEnabled { get; set; } = false;
-//}
