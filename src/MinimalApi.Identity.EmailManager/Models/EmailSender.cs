22db3b9e5e289a2aab932ee92f2b4a795420d388
Angelo Pirola <angelo9871@gmail.com>
2026-01-13 00:06:48 +0100

    Add internal EmailSender class with retry support
    Introduced the internal EmailSender class in the MinimalApi.Identity.EmailManager.Models namespace. This class holds an EmailSending instance and a MaxRetryAttempts property to manage email sending and retry logic.

---
+++ src/MinimalApi.Identity.EmailManager/Models/EmailSender.cs
@@ -0,0 +1,9 @@
+﻿using MinimalApi.Identity.Core.Entities;
+
+namespace MinimalApi.Identity.EmailManager.Models;
+
+internal class EmailSender
+{
+    public EmailSending Email { get; set; } = null!;
+    public int MaxRetryAttempts { get; set; } = 0;
+}
